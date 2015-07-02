0216b07e8e3916ddfc9f622717f671eae42f41a2
Nick Papior Andersen <nickpapior@gmail.com>
2015-07-02 00:11:10 +0000

    bug: setup installation

--- sids/setup.py
+++ sids/setup.py
@@ -6,6 +6,7 @@ def configuration(parent_package='',top_path=None):
     config = Configuration('sids', parent_package, top_path)
     config.add_subpackage('geom')
     config.add_subpackage('io')
+    config.add_subpackage('tb')
     config.make_config_py() # installs __config__.py
     return config
 
