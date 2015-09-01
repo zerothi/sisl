14deed39aebb8f26d86857d4cf3037f042091c4e
Nick Papior <nickpapior@gmail.com>
2015-09-01 16:38:43 +0200

    Prepping for release

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ Operating System :: MacOS
 
 MAJOR               = 0
 MINOR               = 2
-MICRO               = 9
+MICRO               = 10
 ISRELEASED          = False
 VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
