2e6bfec52a2828c4da9f17949229c140a439636a
Nick Papior <nickpapior@gmail.com>
2015-10-30 11:39:40 +0100

    Reverting internal release
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ Operating System :: MacOS
 MAJOR               = 0
 MINOR               = 4
 MICRO               = 0
-ISRELEASED          = True
+ISRELEASED          = False
 VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 def generate_cython():
