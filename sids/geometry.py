bug: ensured normalization of rotation vector
Signed-off-by: Nick Papior <nickpapior@gmail.com>

sids/geometry.py
@@ -623,7 +623,8 @@ class Geometry(SuperCellChild):
              if ``abc`` is in this string the cell will be rotated
              if ``xyz`` is in this string the coordinates will be rotated
         """
-        q = Quaternion(angle,v,degree=degree)
+        vn = np.sum(v**2) ** .5
+        q = Quaternion(angle,v / vn,degree=degree)
         q /= q.norm() # normalize the quaternion
 
         # Rotate by direct call

sids/supercell.py
@@ -170,7 +170,8 @@ class SuperCell(object):
         only : ('abc'), str, optional
              only rotate the designated cell vectors.
         """
-        q = Quaternion(angle,v,degree=degree)
+        vn = np.sum(v**2) ** .5
+        q = Quaternion(angle,v / vn,degree=degree)
         q /= q.norm() # normalize the quaternion
         cell = np.copy(self.cell)
         if 'a' in only: