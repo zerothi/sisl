78cf66142a1b422a86aafbe2df4b2113db02a2b9
Nick Papior <nickpapior@gmail.com>
2015-11-15 23:06:02 +0100

    Prepping for release
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- setup.py
+++ setup.py
@@ -28,8 +28,8 @@ Operating System :: MacOS
 
 MAJOR               = 0
 MINOR               = 4
-MICRO               = 0
-ISRELEASED          = False
+MICRO               = 1
+ISRELEASED          = True
 VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 def generate_cython():
