bug: fixed type of rotation vector
Signed-off-by: Nick Papior <nickpapior@gmail.com>

sids/geometry.py
@@ -620,7 +620,7 @@ class Geometry(SuperCellChild):
              if ``abc`` is in this string the cell will be rotated
              if ``xyz`` is in this string the coordinates will be rotated
         """
-        vn = np.asarray(v)[:]
+        vn = np.asarray(v,dtype=np.float64)[:]
         vn /= np.sum(vn ** 2) ** .5
         q = Quaternion(angle, vn, degree=degree)
         q /= q.norm() # normalize the quaternion

sids/supercell.py
@@ -167,7 +167,7 @@ class SuperCell(object):
         only : ('abc'), str, optional
              only rotate the designated cell vectors.
         """
-        vn = np.asarray(v)[:]
+        vn = np.asarray(v,dtype=np.float64)[:]
         vn /= np.sum(vn ** 2) ** .5
         q = Quaternion(angle, vn, degree=degree)
         q /= q.norm() # normalize the quaternion