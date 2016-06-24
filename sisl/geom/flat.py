eaf6136f362a207890fe92124cf4337358479bff
Nick Papior <nickpapior@gmail.com>
2016-06-24 08:48:54 +0200

    enh: changed alat to bond in honeycomb lattice
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- sisl/geom/flat.py
+++ sisl/geom/flat.py
@@ -10,7 +10,7 @@ from sisl import Atom, Geometry, SuperCell
 __all__ = ['honeycomb', 'graphene']
 
 
-def honeycomb(alat, atom, square=False):
+def honeycomb(bond, atom, square=False):
     """
     Returns a honeycomb geometry with the graphene unit-cell (2 atoms)
     """
@@ -18,29 +18,29 @@ def honeycomb(alat, atom, square=False):
     if square:
         sc = SuperCell(np.array([[3., 0., 0.],
                                  [0., 2 * sq3h, 0.],
-                                 [0., 0., 10.]], np.float64) * alat, nsc=[3, 3, 1])
+                                 [0., 0., 10.]], np.float64) * bond, nsc=[3, 3, 1])
         g = Geometry(np.array([[0., 0., 0.],
                                [0.5, sq3h, 0.],
                                [1.5, sq3h, 0.],
-                               [2., 0., 0.]], np.float64) * alat,
+                               [2., 0., 0.]], np.float64) * bond,
                      atoms=atom, sc=sc)
     else:
         sc = SuperCell(np.array([[1.5, sq3h, 0.],
                                  [1.5, -sq3h, 0.],
-                                 [0., 0., 10.]], np.float64) * alat, nsc=[3, 3, 1])
+                                 [0., 0., 10.]], np.float64) * bond, nsc=[3, 3, 1])
         g = Geometry(np.array([[0., 0., 0.],
-                               [1., 0., 0.]], np.float64) * alat,
+                               [1., 0., 0.]], np.float64) * bond,
                      atoms=atom, sc=sc)
     return g
 
 
-def graphene(alat=1.42, atom=None, square=False):
+def graphene(bond=1.42, atom=None, square=False):
     """
     Returns a geometry with the graphene unit-cell (2 atoms)
     """
     if atom is None:
-        return honeycomb(alat, Atom(Z=6, R=alat * 1.01), square)
-    return honeycomb(alat, atom, square)
+        return honeycomb(bond, Atom(Z=6, R=bond * 1.01), square)
+    return honeycomb(bond, atom, square)
 
 
 if __name__ == "__main__":
