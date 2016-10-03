6ecfadbb6e3652166a24c65a61bc22af72c46dea
Nick Papior <nickpapior@gmail.com>
2016-10-03 08:29:02 +0200

    test: added geom tests, fixed coverage
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- sisl/geom/tests/test_geom.py
+++ sisl/geom/tests/test_geom.py
@@ -12,12 +12,22 @@ import numpy as np
 class Test(object):
 
     def test_basis(self):
+        a = sc(2.52, Atom['Fe'])
         a = bcc(2.52, Atom['Fe'])
+        a = bcc(2.52, Atom['Fe'], square=True)
         a = fcc(2.52, Atom['Au'])
+        a = fcc(2.52, Atom['Au'], square=True)
+        a = hcp(2.52, Atom['Au'])
+        a = hcp(2.52, Atom['Au'], square=True)
 
     def test_flat(self):
         a = graphene()
+        a = graphene(atom='C')
         a = graphene(square=True)
+
+    def test_nanotube(self):
+        a = nanotube(1.42)
+        a = nanotube(1.42, chirality=(3,5))
 
     def test_diamond(self):
         a = diamond()
