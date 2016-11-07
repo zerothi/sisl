746286ea86185a2579d2ba2fe8977a9d5cd788fb
Nick Papior <nickpapior@gmail.com>
2016-11-07 10:35:26 +0100

    enh: changed version.py to info.py
    This makes more sense as the info.py also contains git-revision IDs
    
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- sisl/__init__.py
+++ sisl/__init__.py
@@ -4,10 +4,10 @@ Geometry and tight-binding setups using pure python.
 """
 
 # Import version string and the major, minor, micro as well
-from .version import version as __version__
-from .version import major as __major__
-from .version import minor as __minor__
-from .version import micro as __micro__
+from .info import version as __version__
+from .info import major as __major__
+from .info import minor as __minor__
+from .info import micro as __micro__
 
 # load the most commonly, and basic classes
 # The units contain the SI standard conversions using
