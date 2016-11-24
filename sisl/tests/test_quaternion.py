73d20c164d6f7834b5179a5608e0db89328a066a
Nick Papior <nickpapior@gmail.com>
2016-11-24 22:33:07 +0100

    enh: added comparison to Quaternion and added some tests
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

---
+++ sisl/tests/test_quaternion.py
@@ -0,0 +1,48 @@
+from __future__ import print_function, division
+
+from nose.tools import *
+
+from sisl import Quaternion
+
+import math as m
+import numpy as np
+
+
+class TestQuaternion(object):
+
+    def setUp(self):
+        self.qx = Quaternion(90, [1,0,0])
+        self.qy = Quaternion(90, [0,1,0])
+        self.qz = Quaternion(90, [0,0,1])
+        self.Qx = Quaternion(90, [2,0,0])
+        self.Qy = Quaternion(90, [0,2,0])
+        self.Qz = Quaternion(90, [0,0,2])
+
+    def tearDown(self):
+        del self.qx
+        del self.qy
+        del self.qz
+        del self.Qx
+        del self.Qy
+        del self.Qz
+
+    def test_copy(self):
+        qx = self.qx.copy()
+        assert_equal(qx, self.qx)
+
+    def test_conj(self):
+        qx = self.qx.conj()
+        assert_equal(qx.conj(), self.qx)
+
+    def test_norm(self):
+        for c in 'xyz':
+            assert_equal(getattr(self, 'q'+c).norm(), 1.)
+
+    def test_degree1(self):
+        for c in 'xyz':
+            assert_equal(getattr(self, 'q'+c).degree, 90)
+
+    def test_radians1(self):
+        rx = self.qx.radians
+        ry = self.qy.radians
+        assert_equal(rx, ry)
