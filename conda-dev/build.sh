32b3833ac7e7c12104b8f2becd2002a95fa53220
Nick Papior <nickpapior@gmail.com>
2017-01-06 16:22:28 +0100

    adding conda-dev build (package name sisl-dev)
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- conda-dev/build.sh
+++ conda-dev/build.sh
@@ -5,7 +5,7 @@ git describe --tags > __conda_version__.txt
 a=`cat __conda_version__.txt`
 b=${a//-/}
 if [[ ${#a} -ne ${#b} ]]; then
-   echo "dev" > __conda_version__.txt
+   echo "${a//-/_}" > __conda_version__.txt
 fi
 
 $PYTHON setup.py install
