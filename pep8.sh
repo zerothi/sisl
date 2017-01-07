98a024d80ecc282e82bf4c92990238b3690b1776
Nick Papior <nickpapior@gmail.com>
2017-01-07 16:59:44 +0100

    enh: added the pep8.sh script
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

---
+++ pep8.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script will only do removal of whitespace
+# It relies on the autopep8 tool and the
+# specifics of whitespace error/warnings
+
+# List of errors of whitespace stuff
+select="E101,E20,E211,E231,E301,E302,E303,E304,E309,W291,W293,W391"
+# Errors of comparison with None and bool
+select="$select,E711,E712"
+autopep8 --select "$select" --in-place -r .
