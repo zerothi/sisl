6673284c284431aa9cb42523401ed2146ca7b91b
Nick Papior <nickpapior@gmail.com>
2017-03-13 22:01:21 +0100

    maint: updated pep8 script to apply on NEWS and README.md
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- pep8.sh
+++ pep8.sh
@@ -9,3 +9,6 @@ select="E101,E20,E211,E231,E301,E302,E303,E304,E309,W291,W293,W391"
 # Errors of comparison with None and bool
 select="$select,E711,E712"
 autopep8 --select "$select" --in-place -r .
+
+# Non-Python files
+autopep8 --select "W291,W293" --in-place NEWS README.md
