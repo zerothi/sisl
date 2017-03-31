b8c677207dca11f2a15ec8065f6a42cfc884741f
Nick Papior <nickpapior@gmail.com>
2017-03-31 22:25:55 +0200

    conda: fixed conda-build with new version names
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- conda/build.sh
+++ conda/build.sh
@@ -3,6 +3,9 @@
 # Create the version number
 git describe --tags > __conda_version__.txt
 a=`cat __conda_version__.txt`
+# Convert "vX.Y.Z" to "X.Y.Z"
+a=${a:1}
+echo "$a" > __conda_version__.txt
 b=${a//-/}
 if [[ ${#a} -ne ${#b} ]]; then
    echo "dev" > __conda_version__.txt
