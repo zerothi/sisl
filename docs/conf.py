d1f50b3252447a6363dd6cac6d92d831aa487d9d
Nick Papior <nickpapior@gmail.com>
2017-04-02 22:30:21 +0200

    doc: reverted conf.py for path
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,7 @@ import shlex
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../..'))
 
 # -- General configuration ------------------------------------------------
 
