27f0566a7573f4791379a7922c4095e68a4231dd
Nick Papior <nickpapior@gmail.com>
2017-04-06 10:27:41 +0200

    doc: updated pictures and script
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- docs/tutorials/convert.bash
+++ docs/tutorials/convert.bash
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-for pdf in *.pdf
+for pdf in [0-9]*.pdf
 do
-    convert -density 300 $pdf ${pdf//pdf/png}
+    png=${pdf//pdf/png}
+    if [ ! -e $png ]; then
+	echo "Converting $pdf to $png"
+	convert -density 300 $pdf $png
+	optipng -o7 $png 2>/dev/null >/dev/null
+    fi
 done
 	   
