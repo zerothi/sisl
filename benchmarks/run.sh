4d8cbe44e8db24da5b7f42e0f9aff070982e34c5
Nick Papior <nickpapior@gmail.com>
2017-06-21 21:06:02 +0200

    enh: added run.sh script for the benchmarks dir
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

---
+++ benchmarks/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+script=graphene_100x100.py
+if [ $# -gt 0 ]; then
+    script=$1
+    shift
+fi
+
+# Base name
+base=${script%.py}
+
+# Determine output profile
+profile=$base.profile
+
+# Stats
+stats=$base.stats
+
+python -m cProfile -o $profile $script
+python stats.py $profile > $stats
+
