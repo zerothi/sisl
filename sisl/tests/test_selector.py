a6b0e1f8687beb9c67297868d879515155b7ec03
Nick Papior <nickpapior@gmail.com>
2017-07-06 20:03:05 +0200

    bug: trying to fix the selector tests
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- sisl/tests/test_selector.py
+++ sisl/tests/test_selector.py
@@ -15,7 +15,7 @@ class TestSelector(object):
     def sleep(self, *args):
         if len(args) == 1:
             def _sleep():
-                time.sleep(args[0])
+                time.sleep(1. / 1000 * args[0])
             _sleep.__name__ = str(args[0])
             return _sleep
         return [self.sleep(arg) for arg in args]
@@ -23,64 +23,64 @@ class TestSelector(object):
     def test_selector1(self):
 
         sel = TimeSelector()
-        sel.prepend(self.sleep(0.0001))
-        sel.prepend(self.sleep(0.0002))
-        sel.append(self.sleep(0.0003))
+        sel.prepend(self.sleep(1))
+        sel.prepend(self.sleep(2))
+        sel.append(self.sleep(3))
 
         i = 0
         while sel.best is None:
             sel()
             i += 1
         assert_equal(i, 3)
-        assert_equal(sel.best.__name__, "0.0001")
+        assert_equal(sel.best.__name__, "1")
 
     def test_selector2(self):
-        sel = TimeSelector(self.sleep(0.0001, 0.0002, 0.0003))
+        sel = TimeSelector(self.sleep(1, 2, 3))
 
         while sel.best is None:
             sel()
-        assert_equal(sel.best.__name__, "0.0001")
+        assert_equal(sel.best.__name__, "1")
 
     def test_selector3(self):
-        sel = TimeSelector(self.sleep(0.0003, 0.0002, 0.0001))
+        sel = TimeSelector(self.sleep(3, 2, 1))
 
         i = 0
         while sel.best is None:
             sel()
             i += 1
         assert_equal(i, 3)
-        assert_equal(sel.best.__name__, "0.0001")
+        assert_equal(sel.best.__name__, "1")
 
     def test_ordered1(self):
-        sel = TimeSelector(self.sleep(0.0001, 0.0002, 0.0003), True)
+        sel = TimeSelector(self.sleep(1, 2, 3), True)
 
         i = 0
         while sel.best is None:
             sel()
             i += 1
         assert_equal(i, 2)
-        assert_equal(sel.best.__name__, "0.0001")
+        assert_equal(sel.best.__name__, "1")
         assert_equal(sel.performances[-1], None)
 
     def test_ordered2(self):
-        sel = TimeSelector(self.sleep(0.0001, 0.0002, 0.0003), True)
+        sel = TimeSelector(self.sleep(1, 2, 3), True)
 
         i = 0
         while sel.best is None:
             sel()
             i += 1
         assert_equal(i, 2)
-        assert_equal(sel.best.__name__, "0.0001")
+        assert_equal(sel.best.__name__, "1")
         assert_equal(sel.performances[-1], None)
 
-        sel.prepend(self.sleep(0.0002))
+        sel.prepend(self.sleep(2))
 
         i = 0
         while sel.best is None:
             sel()
             i += 1
         assert_equal(i, 1)
-        assert_equal(sel.best.__name__, "0.0001")
+        assert_equal(sel.best.__name__, "1")
         assert_equal(sel.performances[-1], None)
 
         sel.reset()
@@ -89,22 +89,22 @@ class TestSelector(object):
             sel()
             i += 1
         assert_equal(i, 3)
-        assert_equal(sel.best.__name__, "0.0001")
+        assert_equal(sel.best.__name__, "1")
         assert_equal(sel.performances[-1], None)
 
     def test_select1(self):
-        sel = TimeSelector(self.sleep(0.0001, 0.0002, 0.0003))
+        sel = TimeSelector(self.sleep(1, 2, 3))
 
         i = 0
         while sel.best is None:
             sel()
             i += 1
         assert_equal(i, 3)
-        assert_equal(sel.best.__name__, "0.0001")
+        assert_equal(sel.best.__name__, "1")
 
-        sel.select_best("0.0002")
-        assert_equal(sel.best.__name__, "0.0002")
+        sel.select_best("2")
+        assert_equal(sel.best.__name__, "2")
 
         idx, routine = sel.next()
         assert_equal(idx, -1)
-        assert_equal(routine.__name__, "0.0002")
+        assert_equal(routine.__name__, "2")
