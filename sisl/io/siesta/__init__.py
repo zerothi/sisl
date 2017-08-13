b90bf461190b7a35d37d6e90134ec4c066d1ee99
Nick Papior <nickpapior@gmail.com>
2017-08-13 21:00:47 +0200

    api: moved tbtrans_av implementations to the tbtrans module
    This is easier and less cluthered in the documentation.
    
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- sisl/io/siesta/__init__.py
+++ sisl/io/siesta/__init__.py
@@ -13,7 +13,6 @@ from .out import *
 from .siesta import *
 from .siesta_grid import *
 from .tbtrans import *
-from .tbtrans_av import *
 from .tbtrans_proj import *
 from .xv import *
 
@@ -29,6 +28,5 @@ extendall(__all__, 'sisl.io.siesta.out')
 extendall(__all__, 'sisl.io.siesta.siesta')
 extendall(__all__, 'sisl.io.siesta.siesta_grid')
 extendall(__all__, 'sisl.io.siesta.tbtrans')
-extendall(__all__, 'sisl.io.siesta.tbtrans_av')
 extendall(__all__, 'sisl.io.siesta.tbtrans_proj')
 extendall(__all__, 'sisl.io.siesta.xv')
