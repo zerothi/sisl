349ada4eda295366da4898dfc2530a279be23ed3
Nick Papior <nickpapior@gmail.com>
2017-08-17 21:17:25 +0200

    bug: fixed _siesta module
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- sisl/io/siesta/binaries.py
+++ sisl/io/siesta/binaries.py
@@ -6,7 +6,7 @@ from __future__ import print_function
 import numpy as np
 
 try:
-    import sisl.io.siesta._siesta
+    import sisl.io.siesta._siesta as _siesta
     found_module = True
 except Exception as e:
     print(e)
