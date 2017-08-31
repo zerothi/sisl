051b1a479dc15421194e3ad1257abc46a1a36a6a
Nick Papior <nickpapior@gmail.com>
2017-08-31 19:47:02 +0200

    test: moved grid to pytest
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- sisl/tests/test_grid.py
+++ sisl/tests/test_grid.py
@@ -1,131 +1,129 @@
 from __future__ import print_function, division
 
-from nose.tools import *
-from nose.plugins.attrib import attr
+import pytest
 
 import math as m
 import numpy as np
 
 from sisl import Grid, SuperCell
 
+@pytest.fixture
+def setup():
+    class t():
+        def __init__(self):
+            alat = 1.42
+            sq3h = 3.**.5 * 0.5
+            self.sc = SuperCell(np.array([[1.5, sq3h, 0.],
+                                          [1.5, -sq3h, 0.],
+                                          [0., 0., 10.]], np.float64) * alat, nsc=[3, 3, 1])
+            self.g = Grid([10, 10, 100], sc=self.sc)
+            self.g[:, :, :] = 2.
+            g = Grid(sc=self.sc)
+    return t()
 
-@attr('grid')
+
+@pytest.mark.grid
 class TestGrid(object):
 
-    def setUp(self):
-        alat = 1.42
-        sq3h = 3.**.5 * 0.5
-        self.sc = SuperCell(np.array([[1.5, sq3h, 0.],
-                                      [1.5, -sq3h, 0.],
-                                      [0., 0., 10.]], np.float64) * alat, nsc=[3, 3, 1])
-        self.g = Grid([10, 10, 100], sc=self.sc)
-        self.g[:, :, :] = 2.
-        g = Grid(sc=self.sc)
+    def test_append(self, setup):
+        g = setup.g.append(setup.g, 0)
+        assert np.allclose(g.grid.shape, [20, 10, 100])
+        g = setup.g.append(setup.g, 1)
+        assert np.allclose(g.grid.shape, [10, 20, 100])
+        g = setup.g.append(setup.g, 2)
+        assert np.allclose(g.grid.shape, [10, 10, 200])
 
-    def tearDown(self):
-        del self.sc
-        del self.g
+    def test_set(self, setup):
+        v = setup.g[0, 0, 0]
+        setup.g[0, 0, 0] = 3
+        assert setup.g.grid[0, 0, 0] == 3
+        assert setup.g[0, 0, 0] == 3
+        setup.g[0, 0, 0] = v
+        assert setup.g[0, 0, 0] == v
 
-    def test_append(self):
-        g = self.g.append(self.g, 0)
-        assert_true(np.allclose(g.grid.shape, [20, 10, 100]))
-        g = self.g.append(self.g, 1)
-        assert_true(np.allclose(g.grid.shape, [10, 20, 100]))
-        g = self.g.append(self.g, 2)
-        assert_true(np.allclose(g.grid.shape, [10, 10, 200]))
+    def test_size(self, setup):
+        assert np.allclose(setup.g.grid.shape, [10, 10, 100])
 
-    def test_set(self):
-        v = self.g[0, 0, 0]
-        self.g[0, 0, 0] = 3
-        assert_true(self.g.grid[0, 0, 0] == 3)
-        assert_true(self.g[0, 0, 0] == 3)
-        self.g[0, 0, 0] = v
-        assert_true(self.g[0, 0, 0] == v)
+    def test_item(self, setup):
+        assert np.allclose(setup.g[1:2, 1:2, 2:3], setup.g.grid[1:2, 1:2, 2:3])
 
-    def test_size(self):
-        assert_true(np.allclose(self.g.grid.shape, [10, 10, 100]))
+    def test_dcell(self, setup):
+        assert np.all(setup.g.dcell*setup.g.cell >= 0)
 
-    def test_item(self):
-        assert_true(np.allclose(self.g[1:2, 1:2, 2:3], self.g.grid[1:2, 1:2, 2:3]))
+    def test_dvol(self, setup):
+        assert setup.g.dvol > 0
 
-    def test_dcell(self):
-        assert_true(np.all(self.g.dcell*self.g.cell >= 0))
+    def test_shape(self, setup):
+        assert np.all(setup.g.shape == setup.g.grid.shape)
 
-    def test_dvol(self):
-        assert_true(self.g.dvol > 0)
+    def test_dtype(self, setup):
+        assert setup.g.dtype == setup.g.grid.dtype
 
-    def test_shape(self):
-        assert_true(np.all(self.g.shape == self.g.grid.shape))
+    def test_copy(self, setup):
+        assert setup.g.copy() == setup.g
 
-    def test_dtype(self):
-        assert_true(self.g.dtype == self.g.grid.dtype)
-
-    def test_copy(self):
-        assert_true(self.g.copy() == self.g)
-        assert_false(self.g.copy() != self.g)
-
-    def test_add(self):
-        g = self.g + self.g
-        assert_true(np.allclose(g.grid, (self.g * 2).grid))
-        g = self.g.copy()
+    def test_add(self, setup):
+        g = setup.g + setup.g
+        assert np.allclose(g.grid, (setup.g * 2).grid)
+        g = setup.g.copy()
         g *= 2
-        assert_true(np.allclose(g.grid, (self.g * 2).grid))
-        g = self.g.copy()
+        assert np.allclose(g.grid, (setup.g * 2).grid)
+        g = setup.g.copy()
         g /= 2
-        assert_true(np.allclose(g.grid, (self.g / 2).grid))
+        assert np.allclose(g.grid, (setup.g / 2).grid)
 
-    def test_swapaxes(self):
-        g = self.g.swapaxes(0, 1)
-        assert_true(np.allclose(self.g.cell[0, :], g.cell[1, :]))
-        assert_true(np.allclose(self.g.cell[1, :], g.cell[0, :]))
+    def test_swapaxes(self, setup):
+        g = setup.g.swapaxes(0, 1)
+        assert np.allclose(setup.g.cell[0, :], g.cell[1, :])
+        assert np.allclose(setup.g.cell[1, :], g.cell[0, :])
 
-    def test_interp(self):
-        shape = np.array(self.g.shape, np.int32)
-        g = self.g.interp(shape * 2)
+    def test_interp(self, setup):
+        shape = np.array(setup.g.shape, np.int32)
+        g = setup.g.interp(shape * 2)
         g1 = g.interp(shape)
         # Sadly the interpolation does not work as it really
         # should...
         # One cannot interp down/up and retrieve the same
         # grid... Perhaps this is ok, but not good... :(
-        assert_true(np.allclose(self.g.grid, g1.grid))
+        assert np.allclose(setup.g.grid, g1.grid)
 
-    def test_index1(self):
-        mid = np.array(self.g.shape, np.int32) // 2
-        idx = self.g.index(self.sc.center())
-        assert_true(np.all(mid == idx))
+    def test_index1(self, setup):
+        mid = np.array(setup.g.shape, np.int32) // 2
+        idx = setup.g.index(setup.sc.center())
+        assert np.all(mid == idx)
 
-    def test_sum(self):
+    def test_sum(self, setup):
         for i in range(3):
-            assert_true(self.g.sum(i).shape[i] == 1)
+            assert setup.g.sum(i).shape[i] == 1
 
-    def test_mean(self):
+    def test_mean(self, setup):
         for i in range(3):
-            assert_true(self.g.mean(i).shape[i] == 1)
+            assert setup.g.mean(i).shape[i] == 1
 
-    def test_cross_section(self):
+    def test_cross_section(self, setup):
         for i in range(3):
-            assert_true(self.g.cross_section(1, i).shape[i] == 1)
+            assert setup.g.cross_section(1, i).shape[i] == 1
 
-    def test_remove_part(self):
+    def test_remove_part(self, setup):
         for i in range(3):
-            assert_true(self.g.remove_part(1, i, above=True).shape[i] == 1)
+            assert setup.g.remove_part(1, i, above=True).shape[i] == 1
 
-    def test_sub_part(self):
+    def test_sub_part(self, setup):
         for i in range(3):
-            assert_true(self.g.sub_part(1, i, above=False).shape[i] == 1)
-            assert_true(self.g.sub_part(1, i, above=True).shape[i] == self.g.shape[i] - 1)
+            assert setup.g.sub_part(1, i, above=False).shape[i] == 1
+            assert setup.g.sub_part(1, i, above=True).shape[i] == setup.g.shape[i] - 1
 
-    def test_sub(self):
+    def test_sub(self, setup):
         for i in range(3):
-            assert_true(self.g.sub(1, i).shape[i] == 1)
+            assert setup.g.sub(1, i).shape[i] == 1
         for i in range(3):
-            assert_true(self.g.sub([1, 2], i).shape[i] == 2)
+            assert setup.g.sub([1, 2], i).shape[i] == 2
 
-    def test_remove(self):
+    def test_remove(self, setup):
         for i in range(3):
-            assert_true(self.g.remove(1, i).shape[i] == self.g.shape[i]-1)
+            assert setup.g.remove(1, i).shape[i] == setup.g.shape[i]-1
         for i in range(3):
-            assert_true(self.g.remove([1, 2], i).shape[i] == self.g.shape[i]-2)
+            assert setup.g.remove([1, 2], i).shape[i] == setup.g.shape[i]-2
 
-    def test_argumentparser(self):
-        self.g.ArgumentParser()
+    def test_argumentparser(self, setup):
+        setup.g.ArgumentParser()
