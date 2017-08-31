6948b92b8a48a4b87978e928917a1dbcd1a04499
Nick Papior <nickpapior@gmail.com>
2017-08-31 19:48:25 +0200

    test: moved help to pytest
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- sisl/tests/test_help.py
+++ sisl/tests/test_help.py
@@ -1,7 +1,6 @@
 from __future__ import print_function, division
 
-from nose.tools import *
-from nose.plugins.attrib import attr
+import pytest
 
 import math as m
 import numpy as np
@@ -12,14 +11,14 @@ from sisl._help import array_fill_repeat, get_dtype
 class TestHelp(object):
 
     def test_array_fill_repeat1(self):
-        assert_equal(array_fill_repeat([1], 20).shape[0], 20)
-        assert_equal(array_fill_repeat([1, 2], 20).shape[0], 20)
-        assert_equal(array_fill_repeat(1, 20).shape[0], 20)
+        assert (array_fill_repeat([1], 20).shape[0] == 20
+        assert array_fill_repeat([1, 2], 20).shape[0] == 20
+        assert array_fill_repeat(1, 20).shape[0] == 20
 
-    @raises(ValueError)
+    @pytest.mark.xfail(raises=ValueError)
     def test_array_fill_repeat2(self):
         array_fill_repeat([1, 2, 3], 20)
 
     def test_get_dtype1(self):
-        assert_equal(np.int32, get_dtype(1))
-        assert_equal(np.int64, get_dtype(1, int=np.int64))
+        assert np.int32 == get_dtype(1)
+        assert np.int64 == get_dtype(1, int=np.int64)
