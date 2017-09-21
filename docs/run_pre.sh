95a301c994e8dfb83fc5561104453fd98bd8a5fe
Nick Papior <nickpapior@gmail.com>
2017-09-21 16:00:30 +0200

    doc: removed link creation
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- docs/run_pre.sh
+++ docs/run_pre.sh
@@ -24,27 +24,3 @@ if [ -e ../setup.py ]; then
 elif [ -e ../../setup.py ]; then
     sphinx-apidoc -fMeET -o sisl ../../sisl ../../sisl/**/setup.py ../../sisl/**/tests/*
 fi
-
-# Ensure the links.rst.dummy is EVERYWHERE
-function add_links {
-    for d in `ls -d */ 2>/dev/null` ; do
-	if [ $d == "." ]; then
-	    continue
-	fi
-	if [ $d == ".." ]; then
-	    continue
-	fi
-	if [ -d $d ]; then
-	    pushd $d
-	    add_links
-	    popd
-	fi
-	if [ ! -e links.rst.dummy ]; then
-	    ln -s $root/links.rst.dummy .
-	fi
-    done
-}
-
-# Create sym-links
-root=$(pwd)
-add_links
