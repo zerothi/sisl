bug: fixed angle calculation for supercells
Also changed all radians keywords to rad, this is less verbose,
yet very clear.

Signed-off-by: Nick Papior <nickpapior@gmail.com>

sisl/geometry.py
@@ -1079,7 +1079,7 @@ class Geometry(SuperCellChild):
 
     __rmul__ = __mul__
 
-    def angle(self, atom, dir=(1., 0, 0), ref=None, radians=False):
+    def angle(self, atom, dir=(1., 0, 0), ref=None, rad=False):
         r""" The angle between atom `atom` and the direction `dir`, with possibility of a reference coordinate `ref`
 
         The calculated angle can be written as this
@@ -1099,7 +1099,7 @@ class Geometry(SuperCellChild):
            the direction from which the angle is calculated from, default to ``x``
         ref : int or coordinate, optional
            the reference point from which the vectors are drawn, default to origo
-        radians : bool, optional
+        rad : bool, optional
            whether the returned value is in radians
         """
         xi = self.axyz(ensure_array(atom))
@@ -1107,8 +1107,10 @@ class Geometry(SuperCellChild):
             dir = self.cell[direction(dir), :]
         else:
             dir = ensure_array(dir, np.float64)
-        # Normalize
+        # Normalize so we don't have to have this in the
+        # below formula
         dir /= (dir ** 2).sum() ** .5
+        # Broad-casting
         dir.shape = (1, -1)
 
         if ref is None:
@@ -1119,38 +1121,38 @@ class Geometry(SuperCellChild):
             xi -= ensure_array(ref, np.float64)[None, :]
         nx = (xi ** 2).sum(axis=1) ** .5
         ang = np.where(nx > 1e-6, np.arccos((xi * dir).sum(axis=1) / nx), 0.)
-        if radians:
+        if rad:
             return ang
         return np.degrees(ang)
 
-    def rotatea(self, angle, origo=None, atom=None, only='abc+xyz', radians=False):
+    def rotatea(self, angle, origo=None, atom=None, only='abc+xyz', rad=False):
         """ Rotate around first lattice vector
 
         See Also
         --------
         rotate : generic function of this, this routine calls `rotate` with `v = self.cell[0, :]`
         """
-        return self.rotate(angle, self.cell[0, :], origo, atom, only, radians)
+        return self.rotate(angle, self.cell[0, :], origo, atom, only, rad)
 
-    def rotateb(self, angle, origo=None, atom=None, only='abc+xyz', radians=False):
+    def rotateb(self, angle, origo=None, atom=None, only='abc+xyz', rad=False):
         """ Rotate around second lattice vector
 
         See Also
         --------
         rotate : generic function of this, this routine calls `rotate` with `v = self.cell[1, :]`
         """
-        return self.rotate(angle, self.cell[1, :], origo, atom, only, radians)
+        return self.rotate(angle, self.cell[1, :], origo, atom, only, rad)
 
-    def rotatec(self, angle, origo=None, atom=None, only='abc+xyz', radians=False):
+    def rotatec(self, angle, origo=None, atom=None, only='abc+xyz', rad=False):
         """ Rotate around third lattice vector
 
         See Also
         --------
         rotate : generic function of this, this routine calls `rotate` with `v = self.cell[2, :]`
         """
-        return self.rotate(angle, self.cell[2, :], origo, atom, only, radians)
+        return self.rotate(angle, self.cell[2, :], origo, atom, only, rad)
 
-    def rotate(self, angle, v, origo=None, atom=None, only='abc+xyz', radians=False):
+    def rotate(self, angle, v, origo=None, atom=None, only='abc+xyz', rad=False):
         """ Rotate geometry around vector and return a new geometry
 
         Per default will the entire geometry be rotated, such that everything
@@ -1200,12 +1202,12 @@ class Geometry(SuperCellChild):
         vn /= np.sum(vn ** 2) ** .5
 
         # Prepare quaternion...
-        q = Quaternion(angle, vn, radians=radians)
+        q = Quaternion(angle, vn, rad=rad)
         q /= q.norm()
 
         # Rotate by direct call
         if 'abc' in only:
-            sc = self.sc.rotate(angle, vn, radians=radians, only=only)
+            sc = self.sc.rotate(angle, vn, rad=rad, only=only)
         else:
             sc = self.sc.copy()
 
@@ -2814,7 +2816,7 @@ class Geometry(SuperCellChild):
             def __call__(self, parser, ns, values, option_string=None):
                 # Convert value[0] to the direction
                 # The rotate function expects degree
-                ang = angle(values[0], radians=False, in_radians=False)
+                ang = angle(values[0], rad=False, in_rad=False)
                 d = direction(values[1])
                 if d == 0:
                     v = [1, 0, 0]
@@ -2832,7 +2834,7 @@ class Geometry(SuperCellChild):
 
                 def __call__(self, parser, ns, value, option_string=None):
                     # The rotate function expects degree
-                    ang = angle(value, radians=False, in_radians=False)
+                    ang = angle(value, rad=False, in_rad=False)
                     ns._geometry = ns._geometry.rotate(ang, [1, 0, 0])
             p.add_argument(*opts('--rotate-x', '-Rx'), metavar='ANGLE',
                            action=RotationX,
@@ -2842,7 +2844,7 @@ class Geometry(SuperCellChild):
 
                 def __call__(self, parser, ns, value, option_string=None):
                     # The rotate function expects degree
-                    ang = angle(value, radians=False, in_radians=False)
+                    ang = angle(value, rad=False, in_rad=False)
                     ns._geometry = ns._geometry.rotate(ang, [0, 1, 0])
             p.add_argument(*opts('--rotate-y', '-Ry'), metavar='ANGLE',
                            action=RotationY,
@@ -2852,7 +2854,7 @@ class Geometry(SuperCellChild):
 
                 def __call__(self, parser, ns, value, option_string=None):
                     # The rotate function expects degree
-                    ang = angle(value, radians=False, in_radians=False)
+                    ang = angle(value, rad=False, in_rad=False)
                     ns._geometry = ns._geometry.rotate(ang, [0, 0, 1])
             p.add_argument(*opts('--rotate-z', '-Rz'), metavar='ANGLE',
                            action=RotationZ,

sisl/quaternion.py
@@ -11,12 +11,12 @@ class Quaternion(object):
     Quaternion object to enable easy rotational quantities.
     """
 
-    def __init__(self, angle=0., v=None, radians=False):
+    def __init__(self, angle=0., v=None, rad=False):
         """ Create quaternion object with angle and vector """
-        if not radians:
-            half = angle / 180 * m.pi / 2
-        else:
+        if rad:
             half = angle / 2
+        else:
+            half = angle / 180 * m.pi / 2
         self._v = np.empty([4], np.float64)
         self._v[0] = m.cos(half)
         if v is None:
@@ -45,11 +45,11 @@ class Quaternion(object):
         return m.acos(self._v[0]) * 2. / m.pi * 180.
 
     @property
-    def radians(self):
+    def radian(self):
         """ Returns the angle associated with this quaternion (in radians)"""
         return m.acos(self._v[0]) * 2.
 
-    angle = radians
+    angle = radian
 
     def rotate(self, v):
         """ Rotates 3-dimensional vector ``v`` with the associated quaternion """

sisl/supercell.py
@@ -297,16 +297,16 @@ class SuperCell(object):
         rcell[2, :] = rcell[2, :] / dot(rcell[2, :], cell[2, :])
         return rcell * 2. * np.pi
 
-    def rotatea(self, angle, only='abc', radians=False):
-        return self.rotate(angle, self.cell[0, :], only=only, radians=radians)
+    def rotatea(self, angle, only='abc', rad=False):
+        return self.rotate(angle, self.cell[0, :], only=only, rad=rad)
 
-    def rotateb(self, angle, only='abc', radians=False):
-        return self.rotate(angle, self.cell[1, :], only=only, radians=radians)
+    def rotateb(self, angle, only='abc', rad=False):
+        return self.rotate(angle, self.cell[1, :], only=only, rad=rad)
 
-    def rotatec(self, angle, only='abc', radians=False):
-        return self.rotate(angle, self.cell[2, :], only=only, radians=radians)
+    def rotatec(self, angle, only='abc', rad=False):
+        return self.rotate(angle, self.cell[2, :], only=only, rad=rad)
 
-    def rotate(self, angle, v, only='abc', radians=False):
+    def rotate(self, angle, v, only='abc', rad=False):
         """ Rotates the supercell, in-place by the angle around the vector
 
         One can control which cell vectors are rotated by designating them
@@ -319,14 +319,14 @@ class SuperCell(object):
         v     : array_like [3]
              the vector around the rotation is going to happen
              v = [1,0,0] will rotate in the ``yz`` plane
-        radians : bool, optional
+        rad : bool, optional
              Whether the angle is in radians (True) or in degrees (False)
         only : ('abc'), str, optional
              only rotate the designated cell vectors.
         """
         vn = np.copy(np.asarray(v, dtype=np.float64)[:])
         vn /= np.sum(vn ** 2) ** .5
-        q = Quaternion(angle, vn, radians=radians)
+        q = Quaternion(angle, vn, rad=rad)
         q /= q.norm()  # normalize the quaternion
         cell = np.copy(self.cell)
         if 'a' in only:
@@ -578,7 +578,7 @@ class SuperCell(object):
                 return False
         return True
 
-    def angle(self, i, j, radians=False):
+    def angle(self, i, j, rad=False):
         """ The angle between two of the cell vectors
 
         Parameters
@@ -587,12 +587,12 @@ class SuperCell(object):
            the first cell vector
         j : int
            the second cell vector
-        radians : bool, optional
+        rad : bool, optional
            whether the returned value is in radians
         """
-        n = np.sum(self.cell[[i, j], :]**2) ** .5
-        ang = math.acos(np.sum(self.cell[i, :] * self.cell[j, :]) / n)
-        if radians:
+        n = np.sum(self.cell[[i, j], :]**2, axis=1) ** .5
+        ang = math.acos(np.dot(self.cell[i, :], self.cell[j, :]) / (n[0] * n[1]))
+        if rad:
             return ang
         return math.degrees(ang)
 

sisl/tests/test_geometry.py
@@ -291,7 +291,7 @@ class TestGeometry(object):
         assert np.allclose(-rot.sc.cell, setup.g.sc.cell)
         assert np.allclose(-rot.xyz, setup.g.xyz)
 
-        rot = setup.g.rotate(np.pi, [0, 0, 1], radians=True)
+        rot = setup.g.rotate(np.pi, [0, 0, 1], rad=True)
         rot.sc.cell[2, 2] *= -1
         assert np.allclose(-rot.sc.cell, setup.g.sc.cell)
         assert np.allclose(-rot.xyz, setup.g.xyz)
@@ -307,7 +307,7 @@ class TestGeometry(object):
         assert np.allclose(-rot.sc.cell, setup.g.sc.cell)
         assert np.allclose(rot.xyz, setup.g.xyz)
 
-        rot = setup.g.rotate(np.pi, [0, 0, 1], radians=True, only='abc')
+        rot = setup.g.rotate(np.pi, [0, 0, 1], rad=True, only='abc')
         rot.sc.cell[2, 2] *= -1
         assert np.allclose(-rot.sc.cell, setup.g.sc.cell)
         assert np.allclose(rot.xyz, setup.g.xyz)
@@ -322,7 +322,7 @@ class TestGeometry(object):
         assert np.allclose(rot.sc.cell, setup.g.sc.cell)
         assert np.allclose(-rot.xyz, setup.g.xyz)
 
-        rot = setup.g.rotate(np.pi, [0, 0, 1], radians=True, only='xyz')
+        rot = setup.g.rotate(np.pi, [0, 0, 1], rad=True, only='xyz')
         assert np.allclose(rot.sc.cell, setup.g.sc.cell)
         assert np.allclose(-rot.xyz, setup.g.xyz)
 

sisl/tests/test_supercell.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, division
 
 import pytest
+from pytest import approx
 
 import math as m
 import numpy as np
@@ -94,7 +95,7 @@ class TestSuperCell(object):
         rot.cell[2, 2] *= -1
         assert np.allclose(-rot.cell, setup.sc.cell)
 
-        rot = setup.sc.rotate(m.pi, [0, 0, 1], radians=True)
+        rot = setup.sc.rotate(m.pi, [0, 0, 1], rad=True)
         rot.cell[2, 2] *= -1
         assert np.allclose(-rot.cell, setup.sc.cell)
 
@@ -107,7 +108,7 @@ class TestSuperCell(object):
         rot.cell[2, 2] *= -1
         assert np.allclose(-rot.cell, setup.sc.cell)
 
-        rot = setup.sc.rotatec(m.pi, radians=True)
+        rot = setup.sc.rotatec(m.pi, rad=True)
         rot.cell[2, 2] *= -1
         assert np.allclose(-rot.cell, setup.sc.cell)
 
@@ -120,7 +121,7 @@ class TestSuperCell(object):
         assert np.allclose(rot.cell[0, :], setup.sc.cell[0, :])
         assert np.allclose(-rot.cell[2, 2], setup.sc.cell[2, 2])
 
-        rot = setup.sc.rotateb(m.pi, radians=True)
+        rot = setup.sc.rotateb(m.pi, rad=True)
         assert np.allclose(rot.cell[1, :], setup.sc.cell[1, :])
         assert np.allclose(-rot.cell[2, 2], setup.sc.cell[2, 2])
 
@@ -288,6 +289,25 @@ class TestSuperCell(object):
         g = graphene(orthogonal=True)
         gbig = g.repeat(40, 0).repeat(40, 1)
         assert g.sc.angle(0, 1) == 90
+
+    @pytest.mark.only
+    def test_angle2(self, setup):
+        sc = SuperCell([1, 1, 1])
+        assert sc.angle(0, 1) == 90
+        assert sc.angle(0, 2) == 90
+        assert sc.angle(1, 2) == 90
+        sc = SuperCell([[1, 1, 0],
+                        [1, -1, 0],
+                        [0, 0, 2]])
+        assert sc.angle(0, 1) == 90
+        assert sc.angle(0, 2) == 90
+        assert sc.angle(1, 2) == 90
+        sc = SuperCell([[3, 4, 0],
+                        [4, 3, 0],
+                        [0, 0, 2]])
+        assert sc.angle(0, 1, rad=True) == approx(0.28379, abs=1e-4)
+        assert sc.angle(0, 2) == 90
+        assert sc.angle(1, 2) == 90
 
     @pytest.mark.xfail(raises=ValueError)
     def test_set_nsc1(self, setup):