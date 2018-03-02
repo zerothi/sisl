15fa8c66bb042035cdf12d6c728abefe490fa277
Nick Papior <nickpapior@gmail.com>
2018-03-02 15:04:41 +0100

    enh: added more tests to State objects
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- sisl/physics/tests/test_state.py
+++ sisl/physics/tests/test_state.py
@@ -7,7 +7,7 @@ pytestmark = pytest.mark.state
 import math as m
 import numpy as np
 
-from sisl import State, CState
+from sisl import geom, State, CState
 
 
 def ar(*args):
@@ -30,6 +30,20 @@ def test_state_creation1():
     state_c = state.copy()
     assert len(state) == len(state_c)
     repr(state)
+
+
+def test_state_repr1():
+    state = State(ar(6))
+    repr(state)
+    state = State(ar(6), parent=geom.graphene())
+    repr(state)
+
+
+def test_state_dkind():
+    state = State(ar(6))
+    assert state.dkind == 'f'
+    state = State(ar(6).astype(np.complex128))
+    assert state.dkind == 'c'
 
 
 def test_state_norm1():
@@ -58,10 +72,16 @@ def test_state_outer1():
     state = State(state)
     out = state.outer()
     o = out.copy()
+    o1 = out.copy()
     o.fill(0)
-    for sub in state:
+    o1.fill(0)
+    for i, sub in enumerate(state):
         o += outer(sub.state[0, :])
+        o1 += state.outer(i)
 
+    assert np.allclose(out, o)
+    assert np.allclose(out, o1)
+    o = state.outer(np.arange(len(state)))
     assert np.allclose(out, o)
 
 
@@ -72,8 +92,44 @@ def test_state_toCState1():
     assert state.norm()[0] == pytest.approx(1)
 
 
+def test_state_toCState2():
+    state = State(ar(10).reshape(2, -1)).toCState()
+    assert np.allclose(state.norm(), 1.)
+    state = State(ar(10).reshape(2, -1)).toCState(norm=[0.5, 0.5])
+    assert np.allclose(state.norm() ** 2, 0.5)
+    state = State(ar(10).reshape(2, -1)).toCState(norm=[0.25, 0.75])
+    assert np.allclose(state.norm() ** 2, [0.25, 0.75])
+
+
+@pytest.mark.xfail(raises=ValueError)
+def test_state_toCState_fail():
+    State(ar(10).reshape(2, -1)).toCState(norm=[0.2, 0.5, 0.5])
+
+
 def test_cstate_creation1():
     state = CState(1, ar(6))
+
+
+def test_cstate_repr1():
+    state = CState(1, ar(6))
+    repr(state)
+    state = CState(1, ar(6), parent=geom.graphene())
+    repr(state)
+
+
+def test_cstate_sub1():
+    state = ar(100).reshape(10, -1)
+    state = State(state).toCState().copy()
+    assert len(state) == 10
+    norm = state.norm()
+    for i in range(len(state)):
+        assert len(state.sub(i)) == 1
+        assert state.sub(i).norm()[0] == norm[i]
+        assert state[i].norm()[0] == norm[i]
+        assert state[i].c[0] == state.c[i]
+    for i, sub in enumerate(state):
+        assert len(sub) == 1
+        assert sub.norm()[0] == norm[i]
 
 
 def test_cstate_outer1():
@@ -81,8 +137,14 @@ def test_cstate_outer1():
     state = CState(ar(10), state)
     out = state.outer()
     o = out.copy()
+    o1 = out.copy()
     o.fill(0)
-    for sub in state:
+    o1.fill(0)
+    for i, sub in enumerate(state):
         o += couter(sub.c[0], sub.state[0, :])
+        o1 += state.outer(i)
 
+    assert np.allclose(out, o)
+    assert np.allclose(out, o1)
+    o = state.outer(np.arange(len(state)))
     assert np.allclose(out, o)
