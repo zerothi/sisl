36551354c0ae533fb48eb91c69fba65011b56894
Nick Papior <nickpapior@gmail.com>
2018-04-12 09:58:38 +0200

    test: added more grid tests
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- sisl/io/siesta/tests/test_grid.py
+++ sisl/io/siesta/tests/test_grid.py
@@ -13,3 +13,15 @@ def test_si_pdos_kgrid_grid(sisl_files):
     si = sisl.get_sile(sisl_files(_dir, 'si_pdos_kgrid.VT'))
     grid = si.read_grid()
     assert si.grid_unit == pytest.approx(sisl.unit.siesta.unit_convert('Ry', 'eV'))
+
+
+def test_si_pdos_kgrid_grid_cell(sisl_files):
+    si = sisl.get_sile(sisl_files(_dir, 'si_pdos_kgrid.VT'))
+    si.read_supercell()
+
+
+def test_si_pdos_kgrid_grid_fractions(sisl_files):
+    si = sisl.get_sile(sisl_files(_dir, 'si_pdos_kgrid.VT'))
+    grid = si.read_grid()
+    grid_halve = si.read_grid(spin=[0.5])
+    assert np.allclose(grid.grid * 0.5, grid_halve.grid)
