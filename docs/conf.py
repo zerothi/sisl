52334b4b2189c79afecbb26901d5caf760b63d7f
Nick Papior <nickpapior@gmail.com>
2018-04-16 11:25:00 +0200

    doc: trying to fix IPython download link
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -379,10 +379,10 @@ nbsphinx_timeout = 60
 nbsphinx_prolog = r"""
 {% set docname = env.doc2path(env.docname, base='docs') %}
 
-.. only:: html
+.. raw:: html
 
      <div align="right">
-     Download IPython notebook `here <https://raw.githubusercontent.com/zerothi/sisl/master/{{ docname }}>`_.
+     Download IPython notebook <a href="https://raw.githubusercontent.com/zerothi/sisl/master/{{ docname }}"> here</a>.
      </div>
 
 """
