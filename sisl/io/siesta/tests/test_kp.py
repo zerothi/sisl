d5c8bb201483edfa5f3c631b7b5817b0fa335143
Nick Papior <nickpapior@gmail.com>
2018-06-12 10:46:18 +0200

    bug: fixed unit reading in test for KP
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- sisl/io/siesta/tests/test_kp.py
+++ sisl/io/siesta/tests/test_kp.py
@@ -5,6 +5,7 @@ import pytest
 from sisl import geom
 from sisl.physics import MonkhorstPack
 from sisl.io.siesta.kp import *
+from sisl.unit.siesta import unit_convert
 
 import numpy as np
 
@@ -18,5 +19,5 @@ def test_kp_read_write(sisl_tmp):
     kpSileSiesta(f, 'w').write_data(bz)
 
     kpoints, weights = kpSileSiesta(f).read_data()
-    assert np.allclose(kpoints, bz.k)
+    assert np.allclose(kpoints * unit_convert('Bohr', 'Ang'), bz.k)
     assert np.allclose(weights, bz.weight)
