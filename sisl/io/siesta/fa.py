52a0ef144ad72ca05ccbef6a393a209b5aee04f7
Nick Papior <nickpapior@gmail.com>
2018-06-15 10:17:38 +0200

    enh: enabled writing FA files
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- sisl/io/siesta/fa.py
+++ sisl/io/siesta/fa.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 import numpy as np
 
 # Import sile objects
-from ..sile import add_sile, Sile_fh_open
+from ..sile import add_sile, Sile_fh_open, sile_raise_write
 from .sile import *
 
 
@@ -15,7 +15,7 @@ class faSileSiesta(SileSiesta):
 
     @Sile_fh_open
     def read_data(self):
-        """ Returns forces from the file """
+        """ Reads the forces from the file """
         na = int(self.readline())
 
         f = np.empty([na, 3], np.float64)
@@ -25,8 +25,26 @@ class faSileSiesta(SileSiesta):
         # Units are already eV / Ang
         return f
 
+    @Sile_fh_open
+    def write_data(self, f, fmt='.9e'):
+        """ Write forces to file
+
+        Parameters
+        ----------
+        fmt : str, optional
+           precision of written forces
+        """
+        sile_raise_write(self)
+        na = len(f)
+        self._write('{}\n'.format(na))
+        _fmt = ('{:d}' + (' {:' + fmt + '}') * 3) + '\n'
+
+        for ia in range(na):
+            self._write(_fmt.format(ia + 1, *f[ia, :]))
+
     # Short-cut
     read_force = read_data
+    write_force = write_data
 
 add_sile('FA', faSileSiesta, case=False, gzip=True)
 add_sile('FAC', faSileSiesta, case=False, gzip=True)
--- sisl/io/siesta/tests/test_fa.py
+++ sisl/io/siesta/tests/test_fa.py
@@ -17,3 +17,14 @@ def test_si_pdos_kgrid_fa(sisl_files):
     assert len(fa) == 2
     fa1 = faSileSiesta(f).read_force()
     assert np.allclose(fa, fa1)
+
+
+def test_read_write_fa(sisl_tmp):
+    f = sisl_tmp('test.FA', _dir)
+
+    fa = np.random.rand(10, 3)
+    faSileSiesta(f, 'w').write_force(fa)
+    fa2 = faSileSiesta(f).read_force()
+
+    assert len(fa) == len(fa2)
+    assert np.allclose(fa, fa2)
