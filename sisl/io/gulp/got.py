bug: fixed returned units of Hessians
Signed-off-by: Nick Papior <nickpapior@gmail.com>

sisl/io/gulp/got.py
@@ -1,7 +1,7 @@
 """
 Sile object for reading/writing GULP in/output
 """
-from __future__ import print_function
+from __future__ import print_function, division
 
 import numpy as np
 from numpy import where
@@ -12,8 +12,9 @@ from ..sile import *
 
 from sisl._help import _range as range
 # Import the geometry object
-from sisl import Geometry, Atom, SuperCell
+from sisl import Geometry, Atom, Orbital, SuperCell
 from sisl.physics import Hessian
+from sisl.unit import unit_convert
 
 
 __all__ = ['gotSileGULP']
@@ -94,11 +95,10 @@ class gotSileGULP(SileGULP):
             # Step to either the geometry or
             f, ki, _ = self.step_either([self._keys['sc'], self._keys['geometry']])
             if not f and ki == 0:
-                raise ValueError(
-                    ('SileGULP tries to lookup the SuperCell vectors '
-                     'using key "' + self._keys['sc'] + '". \n'
-                     'Use ".set_supercell_key(...)" to search for different name.\n'
-                     'This could not be found found in file: "' + self.file + '".'))
+                raise ValueError('SileGULP tries to lookup the SuperCell vectors '
+                                 'using key "' + self._keys['sc'] + '". \n'
+                                 'Use ".set_supercell_key(...)" to search for different name.\n'
+                                 'This could not be found found in file: "' + self.file + '".')
             elif f and ki == 0:
                 # supercell
                 self.readline()
@@ -111,12 +111,13 @@ class gotSileGULP(SileGULP):
                 sc = SuperCell(cell)
 
             elif not f and ki == 1:
-                raise ValueError(
-                    ('SileGULP tries to lookup the Geometry coordinates '
-                     'using key "' + self._keys['geometry'] + '". \n'
-                     'Use ".set_geom_key(...)" to search for different name.\n'
-                     'This could not be found found in file: "' + self.file + '".'))
+                raise ValueError('SileGULP tries to lookup the Geometry coordinates '
+                                 'using key "' + self._keys['geometry'] + '". \n'
+                                 'Use ".set_geom_key(...)" to search for different name.\n'
+                                 'This could not be found found in file: "' + self.file + '".')
             elif f and ki == 1:
+
+                orbs = [Orbital(-1, tag=tag) for tag in 'xyz']
 
                 # We skip 5 lines
                 for _ in [0] * 5:
@@ -130,7 +131,7 @@ class gotSileGULP(SileGULP):
                         break
 
                     ls = l.split()
-                    Z.append(Atom(ls[1], orbital=[-1] * 3))
+                    Z.append(Atom(ls[1], orbital=orbs))
                     xyz.append([float(x) for x in ls[3:6]])
 
                 # Convert to array and correct size
@@ -138,15 +139,13 @@ class gotSileGULP(SileGULP):
                 xyz.shape = (-1, 3)
 
                 if len(Z) == 0 or len(xyz) == 0:
-                    raise ValueError(
-                        'Could not read in cell information and/or coordinates')
+                    raise ValueError('Could not read in cell information and/or coordinates')
 
             elif not f:
                 # could not find either cell or geometry
-                raise ValueError(
-                    ('SileGULP tries to lookup the SuperCell or Geometry.\n'
-                     'None succeeded, ensure file has correct format.\n'
-                     'This could not be found found in file: "' + self.file + '".'))
+                raise ValueError('SileGULP tries to lookup the SuperCell or Geometry.\n'
+                                 'None succeeded, ensure file has correct format.\n'
+                                 'This could not be found found in file: "{}".'.format(self.file))
 
         # as the cell may be read in after the geometry we have
         # to wait until here to convert from fractional
@@ -178,32 +177,25 @@ class gotSileGULP(SileGULP):
 
         hessian = kwargs.get('hessian', None)
         if hessian is None:
+            # The output of the Dynamical matrix contains the mass-scaled dynamical matrix
             dyn = self._read_dyn(geom.no, **kwargs)
         else:
+            # The output of the hessian in the Hessian file does not contain the mass-scaling
             dyn = get_sile(hessian, 'r').read_hessian(**kwargs)
 
             if dyn.shape[0] != geom.no:
                 raise ValueError("Inconsistent Hessian file, number of atoms not correct")
 
-            # Perform mass scaling to retrieve the dynamical matrix
-            mass = [geom.atom[ia].mass for ia in range(geom.na)]
-
-            # Construct orbital mass
-            mass = np.array(mass, np.float64).repeat(3)
+            # Construct orbital mass ** (-.5)
+            rmass = 1 / np.array(geom.atoms.mass, np.float64).repeat(3) ** 0.5
 
             # Scale to get dynamical matrix
-            dyn.data[:] /= np.sqrt(mass[dyn.row] * mass[dyn.col])
-
-            # slower, less memory consuming...
-            #for I, ijd in enumerate(zip(dyn.row, dyn.col, dyn.data)):
-            #    dyn.data[I] = ijd[2] / sqrt(mass[ijd[0]] * mass[ijd[1]])
+            dyn.data[:] *= rmass[dyn.row] * rmass[dyn.col]
 
             # clean-up
             del mass
 
         return Hessian.fromsp(geom, dyn)
-
-    read_hamiltonian = read_hessian
 
     def _read_dyn(self, no, **kwargs):
         """ In case the dynamical matrix is read from the file """
@@ -211,7 +203,7 @@ class gotSileGULP(SileGULP):
         from scipy.sparse import lil_matrix
 
         # Default cutoff
-        cutoff = kwargs.get('cutoff', 0.001)
+        cutoff = kwargs.get('cutoff', 1.e-4)
         # Default dtype
         dtype = kwargs.get('dtype', np.float64)
 
@@ -219,11 +211,10 @@ class gotSileGULP(SileGULP):
 
         f, _ = self.step_to(self._keys['dyn'])
         if not f:
-            raise ValueError(
-                ('SileGULP tries to lookup the Dynamical matrix '
-                 'using key "' + self._keys['dyn'] + '". '
-                 'Use .set_dyn_key(...) to search for different name.'
-                 'This could not be found found in file: "' + self.file + '".'))
+            raise ValueError('SileGULP tries to lookup the Dynamical matrix '
+                             'using key "' + self._keys['dyn'] + '". '
+                             'Use .set_dyn_key(...) to search for different name.'
+                             'This could not be found found in file: "{}".'.format(self.file))
 
         # skip 1 line
         self.readline()
@@ -261,8 +252,7 @@ class gotSileGULP(SileGULP):
                     j += 3
                     if j >= no:
                         # Clear those below the cutoff
-                        dyn[i, :] = where(np.abs(dat[:]) >= cutoff,
-                                          dat, 0.)
+                        dyn[i, :] = where(np.abs(dat[:]) >= cutoff, dat, 0.)
 
                         i += 1
                         j = 0
@@ -275,8 +265,9 @@ class gotSileGULP(SileGULP):
         # Convert to CSR matrix format
         dyn = dyn.tocsr()
 
-        # Convert the GULP data to standard units
-        dyn.data *= (521.469 * 1.23981e-4) ** 2
+        # Convert the hessian such that a diagonalization returns eV ^ 2
+        scale = 1.054571800e-34 / unit_convert('Ang', 'm') / (unit_convert('eV', 'J') * unit_convert('amu', 'kg')) ** 0.5
+        dyn.data *= scale ** 2
 
         return dyn
 

sisl/io/gulp/hessian.py
@@ -1,7 +1,7 @@
 """
 Sile object for reading the Hessian matrix written by GULP
 """
-from __future__ import print_function
+from __future__ import print_function, division
 
 import numpy as np
 from scipy.sparse import lil_matrix
@@ -9,6 +9,7 @@ from scipy.sparse import lil_matrix
 # Import sile objects
 from .sile import SileGULP
 from ..sile import *
+from sisl.unit import unit_convert
 
 
 __all__ = ['hessianSileGULP']
@@ -76,6 +77,10 @@ class hessianSileGULP(SileGULP):
 
         # Convert to COO format
         dyn = dyn.tocoo()
+
+        # Convert the hessian such that a diagonalization returns eV ^ 2
+        scale = 1.054571800e-34 / unit_convert('Ang', 'm') / (unit_convert('eV', 'J') * unit_convert('amu', 'kg')) ** 0.5
+        dyn.data *= scale ** 2
 
         return dyn
 

sisl/io/siesta/fdf.py
@@ -828,7 +828,9 @@ class fdfSileSiesta(SileSiesta):
         # Now handle it...
         #  FC(OLD) = (n_displ, 3, 2, na, 3)
         #  FC(NEW) = (n_displ, 3, na, 3)
-        FC = np.average(FC, axis=2)
+        # Convert the hessian such that a diagonalization returns eV ^ 2
+        scale = 1.054571800e-34 / unit_convert('Ang', 'm') / (unit_convert('eV', 'J') * unit_convert('amu', 'kg')) ** 0.5
+        FC = np.average(FC, axis=2) * scale ** 2
 
         # First we need to create the geometry (without the floating atoms)
         geom = self.read_geometry()
@@ -879,7 +881,9 @@ class fdfSileSiesta(SileSiesta):
             H = Hessian(geom)
 
             # Instead of doing the sqrt in all H = FC (below) we do it here
-            m = geom.atoms.mass ** 0.5
+            m = 1 / geom.atoms.mass ** 0.5
+            FC *= m[fc_atoms].reshape(-1, 1, 1, 1)
+            FC *= m.reshape(1, 1, -1, 1)
 
             j_fc_atoms = fc_atoms.view()
             idx = _a.arangei(len(fc_atoms))
@@ -892,9 +896,8 @@ class fdfSileSiesta(SileSiesta):
                     j_fc_atoms = fc_atoms[idx]
 
                 for ja, fja in zip(idx, j_fc_atoms):
-                    M = 1 / (m[fia] * m[fja])
                     for i, j in xyz_xyz:
-                        H[ia*3+i, ja*3+j] = FC[ia, i, fja, j] * M
+                        H[ia*3+i, ja*3+j] = FC[ia, i, fja, j]
                     xyz_xyz.reset()
 
         else:
@@ -919,8 +922,6 @@ class fdfSileSiesta(SileSiesta):
             sc = SuperCell(sc, nsc=nsc)
             geom_small = Geometry(geom.xyz[fc_atoms], geom.atoms[fc_atoms], sc)
             H = Hessian(geom_small)
-            # Instead of doing the sqrt in all H = FC (below) we do it here
-            m = geom_small.atoms.mass ** 0.5
 
             # Now we need to figure out how the atoms are laid out.
             # It *MUST* either be repeated or tiled (preferentially tiled).
@@ -964,6 +965,11 @@ class fdfSileSiesta(SileSiesta):
             #  5. na
             #  6. x, y, z (force components)
             FC.shape = (na_fc, 3, supercell[axis_tiling[0]], supercell[axis_tiling[1]], supercell[axis_tiling[2]], na_fc, 3)
+
+            # After having done this we can easily mass scale all FC components
+            m = 1 / geom_small.atoms.mass ** 0.5
+            FC *= m.reshape(-1, 1, 1, 1, 1, 1, 1)
+            FC *= m.reshape(1, 1, 1, 1, 1, -1, 1)
 
             if fc_atoms[0] != 0:
                 # TODO we could roll the axis such that the displaced atoms moves into the
@@ -1046,9 +1052,8 @@ class fdfSileSiesta(SileSiesta):
                         iter_j_fc_atoms = iter_fc_atoms[dist(ia, aoff + iter_fc_atoms) <= R]
 
                     for ja in iter_j_fc_atoms:
-                        M = 1 / (m[ia] * m[ja])
                         for i, j in xyz_xyz:
-                            H[ia*3+i, joff+ja*3+j] = FC[ia, i, x, y, z, ja, j] * M
+                            H[ia*3+i, joff+ja*3+j] = FC[ia, i, x, y, z, ja, j]
                         xyz_xyz.reset()
 
         # Remove all zeros
@@ -1056,7 +1061,7 @@ class fdfSileSiesta(SileSiesta):
         # Make Hermitian, this may "halve" some elements if the matrix is not
         # symmetric.
         H.make_hermitian()
-        # We again eliminate due to the cutoff.
+        # We again eliminate due to the cutoff (if elements reduced after make_hermitian, will only happen for missing items)
         H.eliminate_zeros(fc_cut)
 
         # TODO, it may be advantegeous to apply Newtons 3rd law and then call make_hermitian again.