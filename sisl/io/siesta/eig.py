c915153ef665352ef53fa96c37758a7a2e2ead3b
Nick Papior <nickpapior@gmail.com>
2018-07-06 11:45:34 +0200

    enh: added DOS calculation to the .EIG file
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- sisl/io/siesta/eig.py
+++ sisl/io/siesta/eig.py
@@ -3,17 +3,62 @@ from __future__ import print_function
 import numpy as np
 
 # Import sile objects
+from sisl.physics import get_distribution
 from sisl.utils import strmap
 from sisl.utils.cmd import default_ArgumentParser, default_namespace
-from ..sile import add_sile, Sile_fh_open
+from sisl.unit.siesta import units
+from ..sile import add_sile, Sile_fh_open, SileError
 from .sile import *
+from .kp import kpSileSiesta
 
 
 __all__ = ['eigSileSiesta']
 
 
 class eigSileSiesta(SileSiesta):
-    """ EIG Siesta file object """
+    """ The eigenvalue file from siesta exposes methods to read and also plot the data using `sdata`
+
+    The .EIG file from Siesta contains the eigenvalues for k-points used during the SCF.
+    Using the command-line utility `sdata` one may plot the eigenvalue spectrum to visualize the
+    spread of eigenvalues.
+
+    .. code:: bash
+
+        sdata siesta.EIG --plot
+        # or to save to png file
+        sdata siesta.EIG --plot eig_spread.png
+
+
+    One may also plot the DOS using the eigenvalues and the k-point weights:
+
+    .. code:: bash
+
+        sdata siesta.EIG --dos
+        # or to save to png file
+        sdata siesta.EIG --dos dos.png
+
+    This will default to plot the DOS using these parameters:
+    dE = 5 meV, kT = 300 K (25 meV), Gaussian distribution and in the full energy-range of the eigenvalue spectrum.
+    By default the k-point weights will be read in the *.KP file, however if the file does not
+    exist one may use the option ``--kp-file FILE`` to read in the weights from ``FILE``.
+
+    To limit the shown energy region, simply use:
+
+    .. code:: bash
+
+        sdata siesta.EIG -E -10:10 --dos
+
+    to reduce to the -10 eV to 10 eV energy range.
+
+    One may optionally choose the temperature smearing and the used distribution function:
+
+    .. code:: bash
+
+        sdata siesta.EIG -E -10:10 --dos 0.01 0.1 lorentzian
+
+    which will calculate the DOS in steps of 10 meV, the temperature smearing is 0.1 eV and
+    the used distribution is a Lorentzian.
+    """
 
     @Sile_fh_open
     def read_data(self):
@@ -87,6 +132,13 @@ class eigSileSiesta(SileSiesta):
                        action=ERange,
                        help='Denote the sub-section of energies that are plotted: "-1:0,1:2" [eV]')
 
+        # k-point weights
+        class KP(argparse.Action):
+            def __call__(self, parser, ns, value, option_string=None):
+                ns._weights = kpSileSiesta(value[0]).read_data()[1]
+        p.add_argument('--kp-file', '-kp', nargs=1, metavar='FILE', action=KP,
+                       help='The k-point file from which to read the band-weights (only applicable to --dos option)')
+
         class EIGPlot(argparse.Action):
 
             def __call__(self, parser, ns, value, option_string=None):
@@ -126,6 +178,75 @@ class eigSileSiesta(SileSiesta):
                     plt.savefig(value)
         p.add_argument(*opts('--plot', '-p'), action=EIGPlot, nargs='?', metavar='FILE',
                        help='Plot the eigenvalues from the .EIG file, possibly saving plot to a file.')
+
+        # Energy grabs
+        class DOSPlot(argparse.Action):
+            def __call__(dosplot, parser, ns, value, option_string=None):
+                import matplotlib.pyplot as plt
+                if not hasattr(ns, '_weight'):
+                    # Try and read in the k-point-weights
+                    ns._weight = kpSileSiesta(self.file.replace('EIG', 'KP')).read_data()[1]
+
+                if ns._Emap is None:
+                    # We will plot the DOS in the entire energy window
+                    ns._Emap = [ns._eigs.min(), ns._eigs.max()]
+
+                if len(ns._weight) != ns._eigs.shape[1]:
+                    raise SileError(str(self) + ' --dos the number of k-points for the eigenvalues and k-point weights '
+                                    'are different, please use --weight correctly.')
+
+                # Specify default settings
+                dE = 0.005 # 5 meV
+                kT = units('K', 'eV') * 300
+                distr = get_distribution('gaussian', smearing=kT)
+                out = None
+                if len(value) > 0:
+                    i = 0
+                    try:
+                        dE = float(value[i])
+                        i += 1
+                    except: pass
+                    try:
+                        kT = float(value[i])
+                        i += 1
+                    except: pass
+                    try:
+                        distr = get_distribution(value[i], smearing=kT)
+                        i += 1
+                    except: pass
+                    try:
+                        out = value[i]
+                    except: pass
+
+                # Now we are ready to process
+                E = np.arange(ns._Emap[0] - kT * 4, ns._Emap[1] + kT * 4, dE)
+
+                def myplot(ax, legend, E, eig, w):
+                    DOS = np.zeros(len(E))
+                    for ib in range(eig.shape[0]):
+                        for e in eig[ib, :]:
+                            DOS += distr(E - e) * w[ib]
+                    ax.plot(E, DOS, label=legend)
+                    ax.set_ylim(0, None)
+
+                plt.figure()
+                ax = plt.gca()
+                ax.set_title('DOS kT={:.1f} K'.format(kT * units('eV', 'K')))
+                ax.set_xlabel('E - Ef [eV]')
+                ax.set_xlim(E.min(), E.max())
+                ax.set_ylabel('DOS [1/eV]')
+                if E.shape[0] == 2:
+                    for i, ud in enumerate(['up', 'down']):
+                        myplot(ax, ud, E, ns._eigs[i, :, :], ns._weight)
+                else:
+                    myplot(ax, '', E, ns._eigs[0, :, :], ns._weight)
+                if out is None:
+                    plt.show()
+                else:
+                    plt.savefig(out)
+        p.add_argument('--dos', action=DOSPlot, nargs='*', metavar='dE,kT,DIST,FILE',
+                       help='Plot the density of states from the .EIG file, '
+                       'dE = energy separation, kT = smearing, DIST = distribution function (Gaussian) possibly saving plot to a file.')
 
         return p, namespace
 
