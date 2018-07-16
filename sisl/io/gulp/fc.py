bug: fixed units for GULP reads
Also added default FC values when reading dynamical matrix from siesta.

Signed-off-by: Nick Papior <nickpapior@gmail.com>

sisl/io/gulp/fc.py
@@ -4,9 +4,9 @@ Sile object for reading the force constant matrix written by GULP
 from __future__ import print_function, division
 
 import numpy as np
+from numpy import abs as np_abs
 from scipy.sparse import lil_matrix
 
-# Import sile objects
 from .sile import SileGULP
 from ..sile import *
 
@@ -25,14 +25,13 @@ class fcSileGULP(SileGULP):
 
         Parameters
         ----------
-        cutoff: float (1e-4 eV/Ang**2)
-           the cutoff of the force-constant matrix for adding to the matrix
+        cutoff : float, optional
+            absolute values below the cutoff are considered 0. Defaults to 1e-4 eV/Ang**2.
         dtype: np.dtype (np.float64)
            default data-type of the matrix
         """
         # Default cutoff
         cutoff = kwargs.get('cutoff', 1e-4)
-
         dtype = kwargs.get('dtype', np.float64)
 
         # Read number of atoms in the file...
@@ -51,8 +50,8 @@ class fcSileGULP(SileGULP):
 
                 # read line that should contain:
                 #  ia ja
-                I, J = map(int, rl().split())
-                if I != ia + 1 or J != ja + 1:
+                lsplit = rl().split()
+                if int(lsplit[0]) != ia + 1 or int(lsplit[1]) != ja + 1:
                     raise ValueError("Inconsistent 2ND file data")
 
                 # Read 3x3 data
@@ -60,16 +59,15 @@ class fcSileGULP(SileGULP):
                     ii = i + o
                     lsplit = rl().split()
                     for oo in [0, 1, 2]:
-                        f = float(lsplit[oo])
-                        # Assign data...
-                        if abs(f) >= cutoff:
-                            fc[ii, j+oo] = f
+                        fc[ii, j+oo] = float(lsplit[oo])
 
                 j += 3
             i += 3
 
         # Convert to COO format
         fc = fc.tocoo()
+        fc.data[np_abs(fc.data) < cutoff] = 0.
+        fc.eliminate_zeros()
 
         return fc
 

sisl/io/gulp/got.py
@@ -6,6 +6,8 @@ from __future__ import print_function, division
 import os.path as osp
 import numpy as np
 from numpy import where
+from numpy import abs as np_abs
+
 
 from sisl.messages import info, warn
 from .sile import SileGULP
@@ -23,7 +25,7 @@ class gotSileGULP(SileGULP):
     """ GULP output file object """
 
     def _setup(self, *args, **kwargs):
-        """ Setup `goutSileGULP` after initialization """
+        """ Setup `gotSileGULP` after initialization """
 
         self._keys = {
             'sc': 'Final Cartesian lattice vectors',
@@ -167,8 +169,8 @@ class gotSileGULP(SileGULP):
 
         Parameters
         ----------
-        cutoff: float (0.001 eV/Ang**2)
-           the cutoff of the force-constant matrix for adding to the matrix
+        cutoff: float, optional
+           absolute values below the cutoff are considered 0. Defaults to 1e-4 eV/Ang**2.
         dtype: np.dtype (np.float64)
            default data-type of the matrix
         order: list of str, optional
@@ -178,7 +180,7 @@ class gotSileGULP(SileGULP):
         """
         geom = self.read_geometry(**kwargs)
 
-        order = kwargs.get('order', ['got', 'FC'])
+        order = kwargs.pop('order', ['got', 'FC'])
         for f in order:
             v = getattr(self, '_r_dynamical_matrix_{}'.format(f.lower()))(geom, **kwargs)
             if v is not None:
@@ -194,9 +196,8 @@ class gotSileGULP(SileGULP):
         # Easier for creation of the sparsity pattern
         from scipy.sparse import lil_matrix
 
-        # Default cutoff
+        # Default cutoff eV / Ang ** 2
         cutoff = kwargs.get('cutoff', 1.e-4)
-        # Default dtype
         dtype = kwargs.get('dtype', np.float64)
 
         no = geom.no
@@ -206,7 +207,7 @@ class gotSileGULP(SileGULP):
         if not f:
             info(self.__class__.__name__ + ' tries to lookup the Dynamical matrix '
                              'using key "' + self._keys['dyn'] + '". '
-                             'Use .set_dyn_key(...) to search for different name.'
+                             'Use .set_dynamical_matrix_key(...) to search for different name.'
                              'This could not be found found in file: "{}".'.format(self.file))
             return None
 
@@ -245,7 +246,7 @@ class gotSileGULP(SileGULP):
                     j += 3
                     if j >= no:
                         # Clear those below the cutoff
-                        dyn[i, :] = where(np.abs(dat[:]) >= cutoff, dat, 0.)
+                        dyn[i, :] = dat[:]
 
                         i += 1
                         j = 0
@@ -255,8 +256,15 @@ class gotSileGULP(SileGULP):
         # clean-up for memory
         del dat
 
-        # Convert to CSR matrix format
-        dyn = dyn.tocsr()
+        # Convert to COO matrix format
+        dyn = dyn.tocoo()
+
+        # Construct mass ** (-.5), so we can check cutoff correctly
+        mass_sqrt = np.array(geom.atoms.mass, np.float64).repeat(3) ** 0.5
+        dyn.data[:] *= mass_sqrt[dyn.row] * mass_sqrt[dyn.col]
+        dyn.data[np_abs(dyn.data) < cutoff] = 0.
+        dyn.data[:] *= 1 / (mass_sqrt[dyn.row] * mass_sqrt[dyn.col])
+        dyn.eliminate_zeros()
 
         return dyn
 
@@ -267,9 +275,9 @@ class gotSileGULP(SileGULP):
         if not osp.isfile(f):
             return None
 
-        dyn = fcSileGULP(f, 'r').read_force_constant(**kwargs)
+        fc = fcSileGULP(f, 'r').read_force_constant(**kwargs)
 
-        if dyn.shape[0] != geom.no:
+        if fc.shape[0] != geom.no:
             warn(self.__class__.__name__ + 'read_dynamical_matrix(FC) inconsistent force constant file, number of atoms not correct!')
             return None
 
@@ -277,9 +285,10 @@ class gotSileGULP(SileGULP):
         rmass = 1 / np.array(geom.atoms.mass, np.float64).repeat(3) ** 0.5
 
         # Scale to get dynamical matrix
-        dyn.data[:] *= rmass[dyn.row] * rmass[dyn.col]
+        fc.data[:] *= rmass[fc.row] * rmass[fc.col]
 
-        return dyn
+        return fc
+
 
 # Old-style GULP output
 add_sile('gout', gotSileGULP, gzip=True)

sisl/io/siesta/fdf.py
@@ -816,8 +816,7 @@ class fdfSileSiesta(SileSiesta):
             cutoff value for the distance of the force-constants (everything farther than
             `cutoff_dist` will be set to 0, unit in Ang.
         cutoff : float, optional
-            cutoff value for the force-constants (absolute values below this value will be set
-            to 0). Unit is eV/Ang**2.
+            absolute values below the cutoff are considered 0. Defaults to 1e-4 eV/Ang**2.
         correct_fc : bool, optional
             correct the FC-matrix by forcing the force on the moved atom to be
             equal to the negative sum of all the others. Default to true.
@@ -851,7 +850,7 @@ class fdfSileSiesta(SileSiesta):
         periodic = geom.nsc > 1
 
         # Cut-off too small values
-        fc_cut = kwargs.get('cutoff', 0.)
+        fc_cut = kwargs.get('cutoff', 1e-4)
         FC = np.where(np.abs(FC) > fc_cut, FC, 0.)
 
         # Convert the force constant such that a diagonalization returns eV ^ 2