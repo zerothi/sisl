e5a11b0bac5e038a5d1e57cac5889bc37b65bf71
Nick Papior <nickpapior@gmail.com>
2019-08-13 20:20:51 +0200

    enh: added manual unrolling for cython
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- sisl/_supercell.pyx
+++ sisl/_supercell.pyx
@@ -23,13 +23,19 @@ cpdef cell_invert(np.ndarray[np.float64_t, ndim=2] cell):
     icell[2, 0] = cell[0, 1] * cell[1, 2] - cell[0, 2] * cell[1, 1]
     icell[2, 1] = cell[0, 2] * cell[1, 0] - cell[0, 0] * cell[1, 2]
     icell[2, 2] = cell[0, 0] * cell[1, 1] - cell[0, 1] * cell[1, 0]
-    cdef int i
-    cdef int j
     cdef double f
-    for i in range(3):
-        f = 1. / (icell[i, 0] * cell[i, 0] + icell[i, 1] * cell[i, 1] + icell[i, 2] * cell[i, 2])
-        for j in range(3):
-            icell[i, j] = icell[i, j] * f
+    f = 1. / (icell[0, 0] * cell[0, 0] + icell[0, 1] * cell[0, 1] + icell[0, 2] * cell[0, 2])
+    icell[0, 0] = icell[0, 0] * f
+    icell[0, 1] = icell[0, 1] * f
+    icell[0, 2] = icell[0, 2] * f
+    f = 1. / (icell[1, 0] * cell[1, 0] + icell[1, 1] * cell[1, 1] + icell[1, 2] * cell[1, 2])
+    icell[1, 0] = icell[1, 0] * f
+    icell[1, 1] = icell[1, 1] * f
+    icell[1, 2] = icell[1, 2] * f
+    f = 1. / (icell[2, 0] * cell[2, 0] + icell[2, 1] * cell[2, 1] + icell[2, 2] * cell[2, 2])
+    icell[2, 0] = icell[2, 0] * f
+    icell[2, 1] = icell[2, 1] * f
+    icell[2, 2] = icell[2, 2] * f
     return icell
 
 
@@ -48,12 +54,18 @@ cpdef cell_reciprocal(np.ndarray[np.float64_t, ndim=2] cell):
     rcell[2, 0] = cell[0, 1] * cell[1, 2] - cell[0, 2] * cell[1, 1]
     rcell[2, 1] = cell[0, 2] * cell[1, 0] - cell[0, 0] * cell[1, 2]
     rcell[2, 2] = cell[0, 0] * cell[1, 1] - cell[0, 1] * cell[1, 0]
-    cdef int i
-    cdef int j
     cdef double twopi = 2 * npmath.PI
     cdef double f
-    for i in range(3):
-        f = twopi / (rcell[i, 0] * cell[i, 0] + rcell[i, 1] * cell[i, 1] + rcell[i, 2] * cell[i, 2])
-        for j in range(3):
-            rcell[i, j] = rcell[i, j] * f
+    f = twopi / (rcell[0, 0] * cell[0, 0] + rcell[0, 1] * cell[0, 1] + rcell[0, 2] * cell[0, 2])
+    rcell[0, j] = rcell[0, j] * f
+    rcell[0, j] = rcell[0, j] * f
+    rcell[0, j] = rcell[0, j] * f
+    f = twopi / (rcell[1, 0] * cell[1, 0] + rcell[1, 1] * cell[1, 1] + rcell[1, 2] * cell[1, 2])
+    rcell[1, j] = rcell[1, j] * f
+    rcell[1, j] = rcell[1, j] * f
+    rcell[1, j] = rcell[1, j] * f
+    f = twopi / (rcell[2, 0] * cell[2, 0] + rcell[2, 1] * cell[2, 1] + rcell[2, 2] * cell[2, 2])
+    rcell[2, j] = rcell[2, j] * f
+    rcell[2, j] = rcell[2, j] * f
+    rcell[2, j] = rcell[2, j] * f
     return rcell
