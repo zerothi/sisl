fd9bd1f6e5b42d6c9e47a386dcae74ddd599f6b3
Pol <pfebrer96@gmail.com>
2020-01-09 00:08:59 +0100

    Proof of concept that custom sessions can also be developed

--- sisl/viz/session.py
+++ sisl/viz/session.py
@@ -105,20 +105,21 @@ class Session(Configurable):
     )
 
     @afterSettingsInit
-    def __init__(self):
+    def __init__(self, **kwargs):
 
         self.id = str(uuid.uuid4())
 
-        self.initWarehouse()
+        self.initWarehouse( kwargs.get("firstTab", False) )
+
+        if callable( getattr(self, "_afterInit", None )):
+            self._afterInit()
         
-    def initWarehouse(self):
+    def initWarehouse(self, firstTab):
 
         self.warehouse = {
             "plots": {},
             "structs": {},
-            "tabs": [
-                {"id": str(uuid.uuid4()), "name": "First Tab", "plots": []}
-            ],
+            "tabs": [],
         }
 
     #-----------------------------------------
@@ -259,12 +260,20 @@ class Session(Configurable):
 
         return self.warehouse["tabs"]
     
-    def addTab(self):
+    def addTab(self, tabName = "New tab", plots = []):
         '''
         Adds a new tab to the session
+
+        Arguments
+        ----------
+        tabName: optional, str ("New tab")
+            The name of the new tab
+        plots: optional, array-like
+            Array of ids (as strings) that identify the plots that you want to put inside your tab.
+            Keep in mind that the plots with these ids must be present in self.plots.
         '''
 
-        newTab = {"id": str(uuid.uuid4()), "name": "New tab", "plots": []}
+        newTab = {"id": str(uuid.uuid4()), "name": tabName, "plots": plots}
 
         self.warehouse["tabs"].append(newTab)
 
