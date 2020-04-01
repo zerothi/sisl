b250ad1886ef1bbd07fbaaf73f07ee9e69371637
Pol <pfebrer96@gmail.com>
2020-04-01 04:08:31 +0200

    Integrated built GUI into sisl

---
+++ sisl/viz/GUI/build/precache-manifest.6433a44b97aa3b9662c7ecc3431f0d5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab3e66db6b4234544884cbccca9d3c9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b39822816f5fd99651ad",
+    "url": "/static/css/2.d7e18693.chunk.css"
+  },
+  {
+    "revision": "735569637a01934d82e6",
+    "url": "/static/css/main.265956f3.chunk.css"
+  },
+  {
+    "revision": "b39822816f5fd99651ad",
+    "url": "/static/js/2.1119873c.chunk.js"
+  },
+  {
+    "revision": "5aeb2582d4b8f40167d6635d86d5af63",
+    "url": "/static/js/2.1119873c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "735569637a01934d82e6",
+    "url": "/static/js/main.92056909.chunk.js"
+  },
+  {
+    "revision": "ddd0398bb3ac6e4ae2c0",
+    "url": "/static/js/runtime-main.0de188ca.js"
+  }
+]);
