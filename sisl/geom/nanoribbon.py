ffb8bf733fa785f729264716b560baa50158b225
Thomas Frederiksen <thomas_frederiksen@ehu.eus>
2020-04-02 17:42:11 +0200

    enh: added edge saturation method (inspired by scripts from Sofia Sanz)

--- sisl/geom/nanoribbon.py
+++ sisl/geom/nanoribbon.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-from math import acos, pi
-from sisl import geom, Atom, Cuboid
+from sisl import geom, Atom, Geometry
 
 __all__ = ['nanoribbon']
 
@@ -17,11 +16,11 @@ def nanoribbon(width, kind='armchair', bond=1.42, atom=None, sat_bond=1.09, sat_
     kind : {'armchair', 'zigzag'}
        type of ribbon
     bond : float, optional
-       bond length between atoms in the honeycomb lattice
+       bond length between atoms in the honeycomb lattice. Defaults to 1.42
     atom : Atom, optional
        atom (or atoms) in the honeycomb lattice. Defaults to ``Atom(6)``
     sat_bond : float, optional
-       bond length to the edge saturation atoms
+       bond length to the edge saturation atoms. A negative value will return a structure without edge decoration. Defaults to 1.09
     sat_atom : Atom, optional
        atom (or atoms) for the edge saturation. If ``None`` no edge saturation is
        applied. Defaults to ``Atom(1)``
@@ -64,13 +63,31 @@ def nanoribbon(width, kind='armchair', bond=1.42, atom=None, sat_bond=1.09, sat_
         ribbon.xyz[:, 1] *= -1
         # Set lattice vectors strictly orthogonal
         ribbon.cell[:] = np.diag([ribbon.cell[1, 0], -ribbon.cell[0, 1], ribbon.cell[2, 2]])
-        # Sort along x-axis
-        ribbon = ribbon.sort()
     else:
         raise ValueError("ribbon: kind must be armchair or zigzag")
 
     # Separate ribbons along y-axis
     ribbon.cell[1, 1] += 10.
+
+    # Edge saturation?
+    if sat_bond > 0:
+        edge_atoms = []
+        # Find atoms with just two neighbors
+        for ia in ribbon:
+            idx = ribbon.close(ia, R=(0.1, bond + 0.1))
+            if len(idx[1]) == 2:
+                a, b = idx[1]
+                v1 = ribbon.axyz(a) - ribbon.xyz[ia, :]
+                v2 = ribbon.axyz(b) - ribbon.xyz[ia, :]
+                # Compute normalized vector p pointing away from the two neighbors
+                p = -1 * (v1 + v2)
+                p /= p.dot(p) ** .5
+                edge_atoms.append(ribbon.xyz[ia, :] + sat_bond * p)
+        edge_atoms = Geometry(edge_atoms, atom=sat_atom)
+        ribbon = ribbon.add(edge_atoms)
+
+    # Sort along x-axis
+    ribbon = ribbon.sort()
 
     # Place ribbon inside unit cell
     ribbon = ribbon.move([-np.min(ribbon.xyz[:, 0]), -np.min(ribbon.xyz[:, 1]), 0])
