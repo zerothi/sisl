6e75a017b0d8e1eb1a3f34577db7d6e1e78987ae
Nick Papior <nickpapior@gmail.com>
2020-04-29 13:04:41 +0200

    maint: changed cutoff in GULP to 0

--- sisl/io/gulp/got.py
+++ sisl/io/gulp/got.py
@@ -174,7 +174,7 @@ class gotSileGULP(SileGULP):
         Parameters
         ----------
         cutoff: float, optional
-           absolute values below the cutoff are considered 0. Defaults to 1e-4 eV/Ang**2.
+           absolute values below the cutoff are considered 0. Defaults to 0. eV/Ang**2.
         dtype: np.dtype (np.float64)
            default data-type of the matrix
         order: list of str, optional
@@ -202,7 +202,7 @@ class gotSileGULP(SileGULP):
         from scipy.sparse import lil_matrix
 
         # Default cutoff eV / Ang ** 2
-        cutoff = kwargs.get('cutoff', 1.e-4)
+        cutoff = kwargs.get('cutoff', 0.)
         dtype = kwargs.get('dtype', np.float64)
 
         nxyz = geometry.no
@@ -210,10 +210,10 @@ class gotSileGULP(SileGULP):
 
         f, _ = self.step_to(self._keys['dyn'])
         if not f:
-            info(self.__class__.__name__ + ' tries to lookup the Dynamical matrix '
-                             'using key "' + self._keys['dyn'] + '". '
-                             'Use .set_dynamical_matrix_key(...) to search for different name.'
-                             'This could not be found found in file: "{}".'.format(self.file))
+            info(f"{self.__class__.__name__}.read_dynamical_matrix tries to lookup the Dynamical matrix "
+                 "using key '{self._keys['dyn']}'. "
+                 "Use .set_dynamical_matrix_key(...) to search for different name."
+                 "This could not be found found in file: {self.file}")
             return None
 
         # skip 1 line
@@ -252,7 +252,7 @@ class gotSileGULP(SileGULP):
         # Convert to COO matrix format
         dyn = dyn.tocoo()
 
-        # Construct mass ** (-.5), so we can check cutoff correctly
+        # Construct mass ** (-.5), so we can check cutoff correctly (in unit eV/Ang**2)
         mass_sqrt = geometry.atoms.mass.repeat(3) ** 0.5
         dyn.data[:] *= mass_sqrt[dyn.row] * mass_sqrt[dyn.col]
         dyn.data[np_abs(dyn.data) < cutoff] = 0.
