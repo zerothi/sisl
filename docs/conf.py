f06d4de47d5c913119e1c42e12181f44b6d058a5
Nick Papior <nickpapior@gmail.com>
2020-06-03 11:20:24 +0200

    doc: cleaned links for intersphinx

--- docs/conf.py
+++ docs/conf.py
@@ -418,10 +418,10 @@ numpydoc_show_class_members = False
 # Python, numpy, scipy and matplotlib specify https as the default objects.inv
 # directory. So please retain these links.
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3', None),
-    'numpy': ('https://docs.scipy.org/doc/numpy', None),
-    'scipy': ('https://docs.scipy.org/doc/scipy/reference', None),
-    'matplotlib': ('https://matplotlib.org', None),
+    'python': ('https://docs.python.org/3/', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'scipy': ('https://docs.scipy.org/doc/scipy/reference/', None),
+    'matplotlib': ('https://matplotlib.org/', None),
     'xarray': ('https://xarray.pydata.org/en/stable/', None),
 }
 
