00f4fdbbe2316f820f5352e2ba29190885bda948
Pol <pfebrer96@gmail.com>
2020-08-03 11:15:09 +0200

    maint: GUI build now on github

---
+++ sisl/viz/plotly/gui/build/precache-manifest.b918ba27c7a7486b63d0cdaeb3a2b2a4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c6e24ea202af8e759f81e9f5159ef6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5d0f28636156bd97a16f",
+    "url": "./static/css/2.e1c6bca1.chunk.css"
+  },
+  {
+    "revision": "ecaf58d051b9dfaf87f0",
+    "url": "./static/css/main.34424dd8.chunk.css"
+  },
+  {
+    "revision": "5d0f28636156bd97a16f",
+    "url": "./static/js/2.1fdae514.chunk.js"
+  },
+  {
+    "revision": "b7e99b8ba4d6785103f5433ac83ba0f5",
+    "url": "./static/js/2.1fdae514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecaf58d051b9dfaf87f0",
+    "url": "./static/js/main.4cc8935e.chunk.js"
+  },
+  {
+    "revision": "2a7e251725549f05a6ec",
+    "url": "./static/js/runtime-main.b88261ea.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "./static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "./static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "./static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "./static/media/MaterialIcons-Regular.e79bfd88.eot"
+  }
+]);
