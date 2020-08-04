b21d819f035e3d449d1ca92c9b491bd107f1273d
Nick Papior <nickpapior@gmail.com>
2020-08-04 09:48:50 +0200

    test: added more tests for categories

--- sisl/geom/category/tests/test_geom_category.py
+++ sisl/geom/category/tests/test_geom_category.py
@@ -10,12 +10,14 @@ pytestmark = [pytest.mark.geom, pytest.mark.category, pytest.mark.geom_category]
 def test_geom_category():
     hBN = honeycomb(1.42, [Atom(5, R=1.43), Atom(7, R=1.43)]) * (10, 11, 1)
 
-    B = AtomZ(5)
+    B = AtomZ([5, 6])
     B2 = AtomNeighbours(2, neigh_cat=B)
     N = AtomZ(7)
     N2 = AtomNeighbours(2, neigh_cat=N)
     B3 = AtomNeighbours(3, neigh_cat=B)
     N3 = AtomNeighbours(3, neigh_cat=N)
+
+    assert B != N
 
     n2 = AtomNeighbours(2)
     n3 = AtomNeighbours(3)
@@ -33,13 +35,34 @@ def test_geom_category_no_r():
     B = AtomZ(5)
     B2 = AtomNeighbours(2, neigh_cat=B, R=1.43)
     N = AtomZ(7)
-    N2 = AtomNeighbours(2, neigh_cat=N, R=(0.01, 1.43))
+    N2 = AtomNeighbours(min=2, max=2, neigh_cat=N, R=(0.01, 1.43))
     PT = PeriodicTable()
     B3 = AtomNeighbours(3, neigh_cat=B, R=lambda atom: (0.01, PT.radius(atom.Z)))
     N3 = AtomNeighbours(3, neigh_cat=N, R=1.43)
+    Nabove3 = AtomNeighbours(min=3, neigh_cat=N, R=1.43)
+
+    assert B != N2
+    assert N2 != N3
 
     n2 = AtomNeighbours(2, R=1.43)
 
     category = (B & B2) ^ (N & N2) ^ (B & B3) ^ (N & N3) ^ n2
 
     cat = category.categorize(hBN)
+
+
+def test_geom_category_even_odd():
+    hBN = honeycomb(1.42, Atom[5, 7]) * (4, 5, 1)
+
+    odd = AtomOdd()
+    even = AtomEven()
+
+    assert odd != even
+    assert even != odd
+
+    cat = (odd | even).categorize(hBN)
+    for i, c in enumerate(cat):
+        if i % 2 == 0:
+            assert c == even
+        else:
+            assert c == odd
