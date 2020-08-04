6e21594c743a93f9a113db2bd4a2a65a74065577
Pol <pfebrer96@gmail.com>
2020-08-04 11:03:03 +0200

    maint: not swallowing import error on sisl.viz load

--- sisl/viz/plotly/__init__.py
+++ sisl/viz/plotly/__init__.py
@@ -15,20 +15,15 @@ Miscellaneous routines
 ======================
 
 """
-try:
+from ._presets import *
+from ._templates import *
+from ._user_customs import import_user_plots, import_user_presets, import_user_sessions, import_user_plugins
 
-    from ._presets import *
-    from ._templates import *
-    from ._user_customs import import_user_plots, import_user_presets, import_user_sessions, import_user_plugins
+from .plot import Plot, Animation, MultiplePlot, SubPlots
+from .plots import *
+from .session import Session
+from .sessions import *
+from .plotutils import load
+from ._plotables import register_plotable
 
-    from .plot import Plot, Animation, MultiplePlot, SubPlots
-    from .plots import *
-    from .session import Session
-    from .sessions import *
-    from .plotutils import load
-    from ._plotables import register_plotable
-
-    from ._express import sx as express
-
-except ModuleNotFoundError:
-    pass
+from ._express import sx as express
