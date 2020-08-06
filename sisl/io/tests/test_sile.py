783288a7f0804b3abe8367d859b280b1a20219bb
Pol <pfebrer96@gmail.com>
2020-08-06 17:19:50 +0200

    maint: avoid opening sile in test

--- sisl/io/tests/test_sile.py
+++ sisl/io/tests/test_sile.py
@@ -1,29 +1,33 @@
 from pathlib import Path
-import pickle
 import pytest
 import tempfile
 
 import sisl
 from sisl.io.sile import __siles
 
+@pytest.fixture(params=["pickle", "dill"])
+def pickle_module(request):
+    return request.param
 
 @pytest.fixture(params=__siles)
 def sile(request):
     sile_cls = request.param
 
-    return sile_cls("test")
+    return sile_cls("test", _open=False)
 
-def test_pickling(sile):
 
+def test_pickling(sile, pickle_module):
+
+    pickle_module = pytest.importorskip(pickle_module)
     tmp_file = tempfile.NamedTemporaryFile(delete=False)
     error = None
 
     try:
         with open(tmp_file.name, "wb") as fh:
-            pickle.dump(sile, fh)
+            pickle_module.dump(sile, fh)
 
         with open(tmp_file.name, "rb") as fh:
-            loaded_sile = pickle.load(fh)
+            loaded_sile = pickle_module.load(fh)
     except Exception as e:
         error = e
 
