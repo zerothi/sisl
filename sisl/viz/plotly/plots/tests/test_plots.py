e3df56f61e368d2eb7cb3414ec67ba170ebaa248
Pol <pfebrer96@gmail.com>
2020-08-06 13:15:43 +0200

    maint: starting to use fixtures to test plot subclasses

--- sisl/viz/plotly/plots/tests/test_plots.py
+++ sisl/viz/plotly/plots/tests/test_plots.py
@@ -5,23 +5,24 @@ These tests check that all plot subclasses fulfill at least the most basic stuff
 More tests should be run on each plot, but these are the most basic ones to
 ensure that at least they do not break basic plot functionality.
 """
+import pytest
 
 from sisl.viz.plotly.tests.test_plot import BasePlotTester
 from sisl.viz.plotly.plots import *
 from sisl.viz.plotly.plotutils import get_plot_classes
 
-# ------------------------------------------------------------
-# Factory that returns a basic functionality test for a class
-# ------------------------------------------------------------
+# Test all plot subclasses with the subclass tester
 
+# The following function basically tells pytest to run TestPlotSubClass
+# once for each plot class. It takes care of setting the PlotClass attribute
+# to the corresponding plot class.
+@pytest.fixture(autouse=True, scope="class", params=get_plot_classes())
+def plot_class(request):
+    request.cls.PlotClass = request.param
 
-def get_basic_functionality_test(PlotSubClass):
+class TestPlotSubClass(BasePlotTester):
 
-    class BasicSubClassTest(BasePlotTester):
-
-        PlotClass = PlotSubClass
-
-        def test_compulsory_methods(self):
+    def test_compulsory_methods(self):
 
             assert hasattr(self.PlotClass, "_set_data")
             assert callable(self.PlotClass._set_data)
@@ -29,20 +30,11 @@ def get_basic_functionality_test(PlotSubClass):
             assert hasattr(self.PlotClass, "_plot_type")
             assert isinstance(self.PlotClass._plot_type, str)
 
-        def test_param_groups(self):
+    def test_param_groups(self):
 
-            plot = self.PlotClass()
+        plot = self.PlotClass()
 
-            for group in plot.param_groups:
-                for key in ("key", "name", "icon", "description"):
-                    assert key in group, f'{self.PlotClass} is missing {key} in parameters group {group}'
-
-    return BasicSubClassTest
-
-# ------------------------------------------------------------
-#               Let's test all plot subclasses
-# ------------------------------------------------------------
-
-for PlotSubClass in get_plot_classes():
-
-    globals()[f"Test{PlotSubClass.__name__}"] = get_basic_functionality_test(PlotSubClass)
+        for group in plot.param_groups:
+            for key in ("key", "name", "icon", "description"):
+                assert key in group, f'{self.PlotClass} is missing {key} in parameters group {group}'
+    
