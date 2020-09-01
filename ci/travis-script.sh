ec7a535c1f021d7dd0af5ebd3dee1667d9583936
Nick Papior <nickpapior@gmail.com>
2020-09-01 10:29:20 +0200

    travis: again

--- ci/travis-script.sh
+++ ci/travis-script.sh
@@ -2,6 +2,19 @@
 
 set -ex
 
+
+# setup env
+if [ -r /usr/lib/libeatmydata/libeatmydata.so ]; then
+    # much faster package installation
+    export LD_PRELOAD='/usr/lib/libeatmydata/libeatmydata.so'
+elif [ -r /usr/lib/*/libeatmydata.so ]; then
+    # much faster package installation
+    export LD_PRELOAD='/usr/$LIB/libeatmydata.so'
+fi
+
+source builds/venv/bin/activate
+
+
 which python
 python --version
 # Check basic import
