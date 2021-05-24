5f97ef4ff907c1aa86f2ac7c07b374817e455bbf
Thomas Frederiksen <thomas_frederiksen@ehu.eus>
2021-05-24 09:20:16 +0200

    enh: linear transformation added to SparseCSR

--- sisl/sparse_geometry.py
+++ sisl/sparse_geometry.py
@@ -133,10 +133,6 @@ class _SparseGeometry(NDArrayOperatorsMixin):
         new._csr = self._csr.copy(dtype=dtype)
         return new
 
-    def convert(self, scale):
-        new = self._csr.convert(scale)
-        return new
-
     @property
     def dim(self):
         """ Number of components per element """
