bug: fixed composite metric calculation

toolbox/siesta/minimizer/_metric_siesta.py
@@ -2,13 +2,15 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 import logging
+from sisl.io import get_sile
+from sisl.utils import direction
 import sisl.io.siesta as io_siesta
 
 from ._path import path_abs, path_rel_or_abs
 from ._metric import Metric
 
 
-__all__ = ["SiestaMetric", "EnergyMetric", "EigenvalueMetric", "StressMetric"]
+__all__ = ["SiestaMetric", "EnergyMetric", "EigenvalueMetric", "ForceMetric", "StressMetric"]
 
 
 _log = logging.getLogger("sisl_toolbox.siesta.pseudo")
@@ -92,7 +94,7 @@ class EnergyMetric(SiestaMetric):
     ----------
     out : str, Path
        the output from a Siesta run
-    energy : func, str, optional
+    energy : callable, str, optional
        an operation to post-process the energy.
        If a `str` it will use the given energy, otherwise the function should accept a single
        dictionary (output from: `sisl.io.siesta.outSileSiesta.read_energy`) and convert that
@@ -129,6 +131,54 @@ class EnergyMetric(SiestaMetric):
         return metric
 
 
+class ForceMetric(SiestaMetric):
+    """ Metric is the force (default maximum), read from the FA file
+
+    Alternatively the metric could be any operation on the forces.
+
+    Parameters
+    ----------
+    file : str, Path
+       the file from which to read the forces
+    force : {'abs.max', 'l2.max'} or callable, optional
+       an operation to post-process the energy.
+       If a `str` it will use the given numpy operation on the flattened force array.
+       dictionary (output from: `sisl.io.siesta.*.read_force`) and convert that
+       to a single metric
+    failure : float, optional
+       in case the output does not contain anything runner fails, then we should return a "fake" metric.
+    """
+
+    def __init__(self, file, force='abs.max', failure=0.):
+        super().__init__(failure)
+        self.file = path_rel_or_abs(file)
+        if isinstance(force, str):
+            force_op = force.split(".")
+            def force(forces):
+                f""" {force_op} metric """
+                out = forces
+                for op in force_op:
+                    if op == "l2":
+                        out = (out ** 2).sum(-1) ** 0.5
+                    else:
+                        out = getattr(np, op)(out)
+                return out
+        if not callable(force):
+            raise ValueError(f"{self.__class__.__name__} requires force to be callable or str")
+        self.force = force
+        try:
+            self._op_doc = self.force.__doc__.strip()
+        except:
+            self._op_doc = "user-defined"
+
+    def metric(self, variables):
+        """ Read the force from the `self.file` in `path` """
+        forces = get_sile(self.file).read_force()
+        metric = self.force(forces)
+        _log.debug(f"metric.force [{self.file}:{self._op_doc}] success {metric}")
+        return metric
+
+
 class StressMetric(SiestaMetric):
     """ Metric is the stress tensor, read from the output file
 
@@ -136,20 +186,21 @@ class StressMetric(SiestaMetric):
     ----------
     out : str, Path
        output from a Siesta run
-    stress : func, optional
+    stress : callable, optional
        function which transforms the stress to a single number (the metric).
        By default it sums the diagonal stress components.
     failure : float, optional
        in case the output does not contain anything runner fails, then we should return a "fake" metric.
     """
 
-    def __init__(self, out, stress=None, failure=2.):
+    def __init__(self, out, stress='ABC', failure=2.):
         super().__init__(failure)
         self.out = path_rel_or_abs(out)
-        if stress is None:
+        if isinstance(stress, str):
+            stress_directions = list(map(direction, stress))
             def stress(stress_matrix):
-                """ diagonal-sum """
-                return np.diag(stress_matrix).sum()
+                f""" {stress_directions} metric """
+                return stress_matrix[stress_directions, stress_directions].sum()
         if not callable(stress):
             raise ValueError(f"{self.__class__.__name__} requires stress to be callable")
         self.stress = stress
@@ -162,7 +213,7 @@ class StressMetric(SiestaMetric):
         """ Convert the stress-tensor to a single metric that should be minimized """
         out = io_siesta.outSileSiesta(self.out)
         if _siesta_out_accept(out):
-            metric = self.stress_op(out.read_stress())
+            metric = self.stress(out.read_stress())
             _log.debug(f"metric.stress [{self.out}:{self._op_doc}] success {metric}")
         else:
             metric = self.failure

toolbox/siesta/minimizer/_variable.py
@@ -1,10 +1,23 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
+from collections.abc import Iterable
 import numpy as np
 
 
-__all__ = ['Variable', 'UpdateVariable']
+__all__ = ['Variable', 'UpdateVariable', 'read_variable_yaml']
+
+
+def read_variable_yaml(dict_or_yaml, name=None):
+    r""" Reads entries in a yaml file """
+    if not isinstance(dict_or_yaml, dict):
+        # Then it must be a yaml file
+        import yaml
+        dict_or_yaml = yaml.load(open(dict_or_yaml, 'r'), Loader=yaml.CLoader)
+    if name is not None:
+        if name in dict_or_yaml:
+            dict_or_yaml = dict_or_yaml[name]
+    return dict_or_yaml
 
 
 class Variable:
@@ -26,6 +39,7 @@ class Variable:
         self.name = name
         self.bounds = np.array(bounds, np.float64)
         assert self.bounds.size == 2
+        assert self.bounds[0] <= self.bounds[1]
         self.value = value
         self.attrs = attrs
 
@@ -37,10 +51,10 @@ class Variable:
 
     def _parse_norm(self, norm, with_offset):
         """ Return offset, scale factor """
-        if isinstance(norm, (tuple, list)):
-            norm, scale = norm
-        elif isinstance(norm, str):
+        if isinstance(norm, str):
             scale = 1.
+        elif isinstance(norm, Iterable):
+            norm, scale = norm
         else:
             scale = norm
             norm = 'l2'
@@ -54,16 +68,14 @@ class Variable:
             return 0., 1.
         elif norm == 'l2':
             return off, scale / (self.bounds[1] - self.bounds[0])
-        elif norm == 'max':
-            return off, scale / np.fabs(self.bounds).max()
-        raise ValueError("norm not found in [none/identity, l2, max]")
+        raise ValueError("norm not found in [none/identity, l2]")
 
     def normalize(self, value, norm='l2', with_offset=True):
         """ Normalize a value in terms of the norms of this variable
 
         Parameters
         ----------
-        norm : {l2, max, none/identity} or (str, float) or float
+        norm : {l2, none/identity} or (str, float) or float
            whether to scale according to bounds or not
            if a scale value (float) is used then that will be the [0, scale] bounds
            of the normalized value, only passing a float is equivalent to ``('l2', scale)``