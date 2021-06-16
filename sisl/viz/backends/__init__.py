ec939e8cadadbe3aa6911d3c8443c91b2a79b805
Pol Febrer <pfebrer96@gmail.com>
2021-06-16 13:18:24 +0200

    enh: template backends

--- sisl/viz/backends/__init__.py
+++ sisl/viz/backends/__init__.py
@@ -1,7 +1,9 @@
 import importlib
 
+__all__ = ["load_backends"]
+
 def load_backends():
-    for backend in ("plotly", "matplotlib"):
+    for backend in ("templates", "plotly", "matplotlib"):
         try:
             importlib.import_module(f".{backend}", __name__)
         except ModuleNotFoundError:
