2875422e145733e5f049ad7ec4d563e318cc4456
Pol Febrer <pfebrer96@gmail.com>
2021-06-29 19:10:04 +0200

    maint: comments in matplotlib bands backend

--- sisl/viz/backends/matplotlib/_plots/bands.py
+++ sisl/viz/backends/matplotlib/_plots/bands.py
@@ -20,43 +20,35 @@ class MatplotlibBandsBackend(MatplotlibBackend, BandsBackend):
     def draw_bands(self, filtered_bands, spin_texture, spin_moments, spin_texture_colorscale, spin_polarized, bands_color, spindown_color, bands_width, spin, add_band_trace_data):
 
         if spin_texture:
+            # Create the normalization for the colorscale of spin_moments.
             spin_texture_norm = Normalize(spin_moments.min(), spin_moments.max())
 
+        # Now loop through all bands to draw them
         for spin_bands, spin in zip(filtered_bands.transpose('spin', 'band', 'k'), filtered_bands.spin.values):
             for band in spin_bands:
+                # Get the xy values for the band
                 x = band.k.values
                 y = band.values
 
-                if spin_texture:
+                # And plot it differently depending on whether we need to display spin texture or not.
+                if not spin_texture:
+                    # The easy case
+                    self.ax.plot(x, y, color=[bands_color, spindown_color][spin], linewidth=bands_width)
+                else:
+                    # The difficult case. This is heavily based on 
+                    # https://matplotlib.org/stable/gallery/lines_bars_and_markers/multicolored_line.html
                     color = spin_moments.sel(band=band.band.values).values
 
-                    # Create a set of line segments so that we can color them individually
-                    # This creates the points as a N x 1 x 2 array so that we can stack points
-                    # together easily to get the segments. The segments array for line collection
-                    # needs to be (numlines) x (points per line) x 2 (for x and y)
                     points = np.array([x, y]).T.reshape(-1, 1, 2)
                     segments = np.concatenate([points[:-1], points[1:]], axis=1)
                     
                     lc = LineCollection(segments, cmap=spin_texture_colorscale, norm=spin_texture_norm)
+                    
                     # Set the values used for colormapping
                     lc.set_array(color)
                     lc.set_linewidth(bands_width)
                     line = self.ax.add_collection(lc)
-                else:
-                    self.ax.plot(x, y, color=[bands_color, spindown_color][spin], linewidth=bands_width)
-
-        #Define the data of the plot as a list of dictionaries {x, y, 'type', 'name'}
-        # self.add_traces(np.ravel([[{
-        #     'type': 'scatter',
-        #     'x': band.k.values,
-        #     'y': (band).values,
-        #     'mode': 'lines',
-        #     'name': "{} spin {}".format(band.band.values, ["up", "down"][spin]) if spin_polarized else str(band.band.values),
-        #     **scatter_additions(band.band.values, spin),
-        #     'hoverinfo':'name',
-        #     "hovertemplate": '%{y:.2f} eV',
-        #     **add_band_trace_data(band, self)
-        #     } for band in spin_bands] for spin_bands, spin in zip(filtered_bands.transpose('spin', 'band', 'k'), filtered_bands.spin.values)]).tolist())
+                    
 
     def draw_gap(self, ks, Es, color, name, **kwargs):
 
@@ -65,19 +57,6 @@ class MatplotlibBandsBackend(MatplotlibBackend, BandsBackend):
         )
 
         self.ax.legend()
-
-        # self.add_trace({
-        #     'type': 'scatter',
-        #     'mode': 'lines+markers+text',
-        #     'x': ks,
-        #     'y': Es,
-        #     'text': [f'Gap: {Es[1] - Es[0]:.3f} eV', ''],
-        #     'marker': {'color': color},
-        #     'line': {'color': color},
-        #     'name': name,
-        #     'textposition': 'top right',
-        #     **kwargs
-        # })
 
     def after_get_figure(self, plot, Erange, spin, spin_texture_colorscale):
         #Add the ticks
