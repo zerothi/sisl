addd66e9a966cf92a1406f4ab73b62729d3e5505
Nick Papior <nickpapior@gmail.com>
2021-12-08 19:50:00 +0100

    mnt: added MPL-2 header for nearly all files

--- sisl/viz/backends/matplotlib/_plots/__init__.py
+++ sisl/viz/backends/matplotlib/_plots/__init__.py
@@ -1,3 +1,6 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 from .bands import MatplotlibBandsBackend
 from .grid import MatplotlibGridBackend
 from .pdos import MatplotlibPDOSBackend
