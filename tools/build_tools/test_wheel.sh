a847467b201dc9160fa73fb1cbb2ddaaee68702d
Nick Papior <nickpapior@gmail.com>
2022-02-03 12:54:34 +0100

    mnt: minor clean-ups here and there

--- tools/build_tools/test_wheel.sh
+++ tools/build_tools/test_wheel.sh
@@ -1,5 +1,13 @@
-PROJECT=$1
-SISL_FILES_TESTS=${PROJECT}/files/tests
+#!/bin/sh
 
-echo SISL_FILES_TESTS=$SISL_FILES_TESTS
-SISL_FILES_TESTS=${SISL_FILES_TESTS} pytest --pyargs sisl
+# Get project path
+PROJECT=$1
+
+if [ -d ${PROJECT}/files/tests ]; then
+    export SISL_FILES_TESTS=${PROJECT}/files/tests
+    echo "will run with sisl-files tests"
+    echo "SISL_FILES_TESTS=$SISL_FILES_TESTS"
+else
+    echo "will not run with sisl-files tests"
+fi
+pytest --pyargs sisl
