5a7fea2aa5184360b26028be20fa5ab22fdd9076
zerothi <zerothi@users.noreply.github.com>
2022-03-23 10:51:02 +0000

    Deploying to gh-pages from @ zerothi/sisl@81c6f5c2d2394d9710982677cc95f79ddb785b93 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+ga29df33',
+    VERSION: '0.1.dev1+g81c6f5c',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
