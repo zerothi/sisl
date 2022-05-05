zerothi/sisl

commit 4db57748dbe358fb647c12d33d4238889bf710d4

mnt: added spin mixing for polarized calculation in berry
Berry-phase calculations will process the two overlap matrices for
each spin index to obtain a complete picture.

sisl/physics/electron.py
@@ -1005,6 +1005,11 @@ def berry_phase(contour, sub=None, eigvals=False, closed=True, method="berry",
     where :math:`\langle \psi_{k_i} | \psi_{k_{i+1}} \rangle` may be exchanged with an overlap matrix
     of the investigated bands.
 
+    For polarized calculations the overlap matrix will be constructed as
+    :math:`\mathbf S=\mathbf S_\alpha \mathbf S_\beta` where greek letters represent the two spin indices.
+    If passing ``spin`` in the `eigenstate_kwargs` one will only get the data for that :math:`\alpha`
+    spin.
+
     Parameters
     ----------
     contour : BrillouinZone
@@ -1158,7 +1163,11 @@ def berry_phase(contour, sub=None, eigvals=False, closed=True, method="berry",
             return prd
 
     # Do the actual calculation of the final matrix
-    S = _berry(contour.apply.iter.eigenstate(**eigenstate_kwargs))
+    if contour.parent.spin.is_polarized and "spin" not in eigenstate_kwargs:
+        S = (_berry(contour.apply.iter.eigenstate(spin=0, **eigenstate_kwargs)) @
+             _berry(contour.apply.iter.eigenstate(spin=1, **eigenstate_kwargs)))
+    else:
+        S = _berry(contour.apply.iter.eigenstate(**eigenstate_kwargs))
 
     # Get the angle of the berry-phase
     # When using np.angle the returned value is in ]-pi; pi]

commit ed1cf88b89c849325007a55a55896ed28f264ea8

bug: removed 4db57748d, it is wrong
The code introduced in 4db57748d is clearly wrong.

It should not have been there.

I have tried these different things, and all yield non-pi values for
a non-trivial case study. So I think they do not work as intended.

if contour.parent.spin.is_polarized and "spin" not in eigenstate_kwargs:
1)

S = _berry(contour.eigenstate(spin=0)) @ _berry(contour.eigenstate(spin=1))

This equals the full overlap matrix for spin up times
the full overlap matrix for the spin down.

2)

def mix_spins(eigs0, eigs1):
    for eig0, eig1 in zip(eigs0, eigs1):
    	# concatenate the two
	yield eig0
	yield eig1

S = _berry(mix_spins(
            contour.eigenstate(spin=0, **eigenstate_kwargs),
            contour.eigenstate(spin=1, **eigenstate_kwargs)))

This is the same as doing the same k for different spin channels.
This might seem obvious to be wrong, but I tried it.

3)
I thought we could append the two different spins
as though they were additional eigenstates
if sub is not None:
   sub = np.asarray(sub)
   # now add the other ones
   sub = np.concatenate((sub, sub + len(contour.parent))).ravel()
def mix_spins(eigs0, eigs1):
    for eig0, eig1 in zip(eigs0, eigs1):
    	# concatenate the two
	eig0.state = np.concatenate((eig0.state, eig1.state), axis=0)
	eig0.c = np.concatenate((eig0.c, eig1.c), axis=0)
	yield eig0

4)
Same as 3) but changing how the processing works:

def _process(a, b):
    return dot(a, 0.5*(b[:n, :n] + b[n:, n:]))

sisl/physics/electron.py
@@ -1005,11 +1005,6 @@ def berry_phase(contour, sub=None, eigvals=False, closed=True, method="berry",
     where :math:`\langle \psi_{k_i} | \psi_{k_{i+1}} \rangle` may be exchanged with an overlap matrix
     of the investigated bands.
 
-    For polarized calculations the overlap matrix will be constructed as
-    :math:`\mathbf S=\mathbf S_\alpha \mathbf S_\beta` where greek letters represent the two spin indices.
-    If passing ``spin`` in the `eigenstate_kwargs` one will only get the data for that :math:`\alpha`
-    spin.
-
     Parameters
     ----------
     contour : BrillouinZone
@@ -1095,10 +1090,12 @@ def berry_phase(contour, sub=None, eigvals=False, closed=True, method="berry",
         def _lowdin(state):
             pass
     else:
+        gauge = eigenstate_kwargs.get("gauge", "R")
         def _lowdin(state):
             """ change state to the lowdin state, assuming everything is in R gauge
             So needs to be done before changing gauge """
-            S12 = sqrth(state.parent.Sk(state.info["k"], format="array"),
+            S12 = sqrth(state.parent.Sk(state.info["k"],
+                                        gauge=gauge, format="array"),
                         overwrite_a=True)
             state.state[:, :] = (S12 @ state.state.T).T
 
@@ -1148,6 +1145,7 @@ def berry_phase(contour, sub=None, eigvals=False, closed=True, method="berry",
 
     else:
         def _berry(eigenstates):
+            nonlocal sub
             first = next(eigenstates)
             _lowdin(first)
             first.sub(sub, inplace=True)
@@ -1162,12 +1160,7 @@ def berry_phase(contour, sub=None, eigvals=False, closed=True, method="berry",
                 prd = _process(prd, prev.inner(first, diag=False))
             return prd
 
-    # Do the actual calculation of the final matrix
-    if contour.parent.spin.is_polarized and "spin" not in eigenstate_kwargs:
-        S = (_berry(contour.apply.iter.eigenstate(spin=0, **eigenstate_kwargs)) @
-             _berry(contour.apply.iter.eigenstate(spin=1, **eigenstate_kwargs)))
-    else:
-        S = _berry(contour.apply.iter.eigenstate(**eigenstate_kwargs))
+    S = _berry(contour.apply.iter.eigenstate(**eigenstate_kwargs))
 
     # Get the angle of the berry-phase
     # When using np.angle the returned value is in ]-pi; pi]