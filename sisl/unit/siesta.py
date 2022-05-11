26668e73238d40e2bcdf73f55cbcbb88ee2ce09c
Nick Papior <nickpapior@gmail.com>
2022-05-11 13:10:09 +0200

    mnt: str conversion to double quote

--- sisl/unit/siesta.py
+++ sisl/unit/siesta.py
@@ -16,43 +16,43 @@ from .base import unit_group as u_group
 from .base import unit_convert as u_convert
 from .base import unit_default as u_default
 
-__all__  = ['unit_group', 'unit_convert', 'unit_default', 'units']
+__all__  = ["unit_group", "unit_convert", "unit_default", "units"]
 
 
 unit_table_siesta = dict({key: dict(values) for key, values in unit_table.items()})
 
-unit_table_siesta['length'].update({
-    'Bohr': 0.529177e-10,
+unit_table_siesta["length"].update({
+    "Bohr": 0.529177e-10,
 })
 
-unit_table_siesta['time'].update({
-    'mins': 60.,
-    'hours': 3600.,
-    'days': 86400.,
+unit_table_siesta["time"].update({
+    "mins": 60.,
+    "hours": 3600.,
+    "days": 86400.,
 })
 
-unit_table_siesta['energy'].update({
-    'meV': 1.60219e-22,
-    'eV': 1.60219e-19,
-    'mRy': 2.17991e-21,
-    'Ry': 2.17991e-18,
-    'mHa': 4.35982e-21,
-    'Ha': 4.35982e-18,
-    'Hartree': 4.35982e-18,
-    'K': 1.38066e-23,
-    'kJ/mol': 1.6606e-21,
-    'Hz': 6.6262e-34,
-    'THz': 6.6262e-22,
-    'cm-1': 1.986e-23,
-    'cm**-1': 1.986e-23,
-    'cm^-1': 1.986e-23,
+unit_table_siesta["energy"].update({
+    "meV": 1.60219e-22,
+    "eV": 1.60219e-19,
+    "mRy": 2.17991e-21,
+    "Ry": 2.17991e-18,
+    "mHa": 4.35982e-21,
+    "Ha": 4.35982e-18,
+    "Hartree": 4.35982e-18,
+    "K": 1.38066e-23,
+    "kJ/mol": 1.6606e-21,
+    "Hz": 6.6262e-34,
+    "THz": 6.6262e-22,
+    "cm-1": 1.986e-23,
+    "cm**-1": 1.986e-23,
+    "cm^-1": 1.986e-23,
 })
 
-unit_table_siesta['force'].update({
-    'eV/Ang': 1.60219e-9,
-    'eV/Bohr': 1.60219e-9*0.529177,
-    'Ry/Bohr': 4.11943e-8,
-    'Ry/Ang': 4.11943e-8/0.529177,
+unit_table_siesta["force"].update({
+    "eV/Ang": 1.60219e-9,
+    "eV/Bohr": 1.60219e-9*0.529177,
+    "Ry/Bohr": 4.11943e-8,
+    "Ry/Ang": 4.11943e-8/0.529177,
 })
 
 
