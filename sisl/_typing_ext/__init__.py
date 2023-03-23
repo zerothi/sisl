76926d89229a628059c7d950349e59d61e205186
Nick Papior <nickpapior@gmail.com>
2023-03-23 11:33:34 +0100

    added license notification at the top of files
    Some files was missing this
    
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- sisl/_typing_ext/__init__.py
+++ sisl/_typing_ext/__init__.py
@@ -1,3 +1,6 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 """ Contains external typing information for local sisl usage
 
 The intended audience for this submodule is that the represented
--- sisl/nodes/__init__.py
+++ sisl/nodes/__init__.py
@@ -1,3 +1,6 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 from .node import Node
 from .workflow import Workflow
 from .context import NodeContext, SISL_NODES_CONTEXT, temporal_context
