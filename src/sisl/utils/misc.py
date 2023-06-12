2476197d1c4ebe5ffdde217397476026f375a905
Nick Papior <nickpapior@gmail.com>
2023-06-12 22:12:11 +0200

    Revert "PropertyDict now inherits from UserDict"
    This reverts commit 0e6f091a69c51419fc0a365579afaa49b9bc34fb.

--- src/sisl/utils/misc.py
+++ src/sisl/utils/misc.py
@@ -9,7 +9,6 @@ import operator as op
 from numbers import Integral
 from math import pi
 import importlib
-from collections import UserDict
 
 
 __all__ = ["merge_instances", "str_spec", "direction", "angle"]
@@ -405,7 +404,7 @@ def lazy_import(name, package=None):
     return module
 
 
-class PropertyDict(UserDict):
+class PropertyDict(dict):
     """ Simple dictionary which may access items as properties as well """
 
     def __getattr__(self, name):
