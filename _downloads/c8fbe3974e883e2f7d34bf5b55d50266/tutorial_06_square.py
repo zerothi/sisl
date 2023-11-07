8b189eff4e27e5adeaac019a64f87f3e74af9f8b
zerothi <zerothi@users.noreply.github.com>
2023-11-07 14:47:49 +0000

    Deploying to gh-pages from @ zerothi/sisl@303aea720db49cdb1e2f3f434b5ccc55b0e257e8 🚀

--- _downloads/c8fbe3974e883e2f7d34bf5b55d50266/tutorial_06_square.py
+++ _downloads/c8fbe3974e883e2f7d34bf5b55d50266/tutorial_06_square.py
@@ -4,9 +4,8 @@
 from sisl import *
 
 # Generate square lattice with nearest neighbour couplings
-Hydrogen = Atom(1, R=1.)
-square = Geometry([[0.5, 0.5, 0]], Hydrogen,
-                  lattice=Lattice([1, 1, 10], [3, 3, 1]))
+Hydrogen = Atom(1, R=1.0)
+square = Geometry([[0.5, 0.5, 0]], Hydrogen, lattice=Lattice([1, 1, 10], [3, 3, 1]))
 
 # Generate Hamiltonian
 H = Hamiltonian(square)
@@ -18,14 +17,14 @@ print(H)
 for ias, idxs in square.iter_block():
     for ia in ias:
         idx_a = square.close(ia, R=[0.1, 1.1], atoms=idxs)
-        H[ia, idx_a[0]] = -4.
-        H[ia, idx_a[1]] = 1.
+        H[ia, idx_a[0]] = -4.0
+        H[ia, idx_a[1]] = 1.0
 
 # Show that we indeed have added some code
 print(H)
 
 # Create band-structure for the supercell.
-band = BandStructure(H, [[0., 0.], [0.5, 0.], [0.5, 0.5], [0., 0.]], 300)
+band = BandStructure(H, [[0.0, 0.0], [0.5, 0.0], [0.5, 0.5], [0.0, 0.0]], 300)
 
 # Calculate eigenvalues of the band-structure
 eigs = band.eigh()
@@ -34,12 +33,12 @@ eigs = band.eigh()
 import matplotlib.pyplot as plt
 
 plt.figure()
-plt.title('Bandstructure of square, nearest neighbour')
-plt.xlabel('k')
-plt.ylabel('Eigenvalue')
+plt.title("Bandstructure of square, nearest neighbour")
+plt.xlabel("k")
+plt.ylabel("Eigenvalue")
 
 # Generate linear-k for plotting (ensures correct spacing)
 lband = band.lineark()
 for i in range(eigs.shape[1]):
     plt.plot(lband, eigs[:, i])
-plt.savefig('06_square_bs.png')
+plt.savefig("06_square_bs.png")
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1-f3633fb',
+    VERSION: '0.1-303aea7',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
