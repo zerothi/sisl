fixed units argument data-type
With type-hinting it is more obvious
what is allowed.

Also fixed all ORCA and VASP return types.

Moved all units serializations to the top
of the method.

I tend to prefer to do options parsing
first, to sustain a better overview of
what the method is doing.

Signed-off-by: Nick Papior <nickpapior@gmail.com>

src/sisl/io/vasp/locpot.py
@@ -7,6 +7,7 @@ import numpy as np
 
 from sisl import Grid
 from sisl._internal import set_module
+from sisl.typing import UnitsVar
 from sisl.unit import serialize_units_arg, unit_convert
 
 from .._help import grid_reduce_indices
@@ -25,7 +26,7 @@ class locpotSileVASP(carSileVASP):
     """
 
     @sile_fh_open(True)
-    def read_grid(self, index=0, dtype=np.float64, units="eV", **kwargs):
+    def read_grid(self, index=0, dtype=np.float64, units: UnitsVar = "eV", **kwargs):
         """Reads the potential from the file and returns with a grid (plus geometry)
 
         Parameters
@@ -38,21 +39,22 @@ class locpotSileVASP(carSileVASP):
            contributions for each corresponding index.
         dtype : numpy.dtype, optional
            grid stored dtype
-        units : {str, dict, list, tuple}
+        units :
            selects units in the returned data
         spin : optional
            same as `index` argument. `spin` argument has precedence.
 
         Returns
         -------
-        Grid : potential with associated geometry
+        out: Grid
+            potential with associated geometry
         """
+        units = serialize_units_arg(units)
+        eV2unit = unit_convert("eV", units["energy"])
+
         index = kwargs.get("spin", index)
         geom = self.read_geometry()
         V = geom.lattice.volume
-
-        units = serialize_units_arg(units)
-        eV2unit = unit_convert("eV", units["energy"])
 
         # Now we are past the cell and geometry
         # We can now read the size of LOCPOT