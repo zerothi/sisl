33acd30ec2a3d0486534948a789afde0121ecafe
Nick Papior <nickpapior@gmail.com>
2024-06-20 09:38:27 +0200

    trying to fix gfortran install
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- tools/build_tools/build_wheels.sh
+++ tools/build_tools/build_wheels.sh
@@ -15,52 +15,71 @@ set -x
 export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
 export PYTHONHASHSEED=0
 
+# List current conda packages
+conda list
+
+# We have to specify which gfortran we should install
+conda_gfortran=gfortran
+
 # OpenMP is not present on macOS by default
 if [[ $(uname) == "Darwin" ]]; then
-    # Make sure to use a libomp version binary compatible with the oldest
-    # supported version of the macos SDK as libomp will be vendored into the
-    # scikit-learn wheels for macos.
+  # Make sure to use a libomp version binary compatible with the oldest
+  # supported version of the macos SDK as libomp will be vendored into the
+  # scikit-learn wheels for macos.
 
-    if [[ "$CIBW_BUILD" == *-macosx_arm64 ]]; then
-        # SciPy requires 12.0 on arm to prevent kernel panics
-        # https://github.com/scipy/scipy/issues/14688
-        # We use the same deployment target to match SciPy.
-        export MACOSX_DEPLOYMENT_TARGET=12.0
-        OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-arm64/llvm-openmp-11.1.0-hf3c4609_1.tar.bz2"
-    else
-        export MACOSX_DEPLOYMENT_TARGET=10.9
-        OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-64/llvm-openmp-11.1.0-hda6cdc1_1.tar.bz2"
-    fi
+  if [[ "$CIBW_BUILD" == *-macosx_arm64 ]]; then
+    # SciPy requires 12.0 on arm to prevent kernel panics
+    # https://github.com/scipy/scipy/issues/14688
+    # We use the same deployment target to match SciPy.
+    export MACOSX_DEPLOYMENT_TARGET=12.0
+    OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-arm64/llvm-openmp-11.1.0-hf3c4609_1.tar.bz2"
 
-    sudo conda create -n build $OPENMP_URL
-    PREFIX="$CONDA_HOME/envs/build"
+  else
+    export MACOSX_DEPLOYMENT_TARGET=10.9
+    OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-64/llvm-openmp-11.1.0-hda6cdc1_1.tar.bz2"
+  fi
 
-    export CC=/usr/bin/clang
-    export CXX=/usr/bin/clang++
-    #export FC=/usr/bin/gfortran
-    export CPPFLAGS="$CPPFLAGS -Xpreprocessor -fopenmp"
-    export CFLAGS="$CFLAGS -I$PREFIX/include"
-    export FFLAGS="$FFLAGS -I$PREFIX/include"
-    export CXXFLAGS="$CXXFLAGS -I$PREFIX/include"
-    export LDFLAGS="$LDFLAGS -Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -lomp"
+  sudo conda create -n build $OPENMP_URL
+  PREFIX="$CONDA_HOME/envs/build"
+
+  export CC=/usr/bin/clang
+  export CXX=/usr/bin/clang++
+  #export FC=/usr/bin/gfortran
+  export CPPFLAGS="$CPPFLAGS -Xpreprocessor -fopenmp"
+  export CFLAGS="$CFLAGS -I$PREFIX/include"
+  export FFLAGS="$FFLAGS -I$PREFIX/include"
+  export CXXFLAGS="$CXXFLAGS -I$PREFIX/include"
+  export LDFLAGS="$LDFLAGS -Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -lomp"
+elif [[ $(uname) == "Linux" ]]; then
+  if [[ $(uname -m) == "x86_64" ]]; then
+    conda_gfortran=gfortran_linux-64
+  else
+    conda_gfortran=gfortran_linux-aarch64
+  fi
 fi
 
 
 if [[ "$GITHUB_EVENT_NAME" == "schedule" \
         || "$GITHUB_EVENT_NAME" == "workflow_dispatch" \
         || "$CIRRUS_CRON" == "nightly" ]]; then
-    # Nightly build:  See also `../github/upload_anaconda.sh` (same branching).
-    # To help with NumPy 2.0 transition, ensure that we use the NumPy 2.0
-    # nightlies.  This lives on the edge and opts-in to all pre-releases.
-    # That could be an issue, in which case no-build-isolation and a targeted
-    # NumPy install may be necessary, instead.
-    export CIBW_BUILD_FRONTEND='pip; args: --pre --extra-index-url "https://pypi.anaconda.org/scientific-python-nightly-wheels/simple"'
+  # Nightly build:  See also `../github/upload_anaconda.sh` (same branching).
+  # To help with NumPy 2.0 transition, ensure that we use the NumPy 2.0
+  # nightlies.  This lives on the edge and opts-in to all pre-releases.
+  # That could be an issue, in which case no-build-isolation and a targeted
+  # NumPy install may be necessary, instead.
+  export CIBW_BUILD_FRONTEND='pip; args: --pre --extra-index-url "https://pypi.anaconda.org/scientific-python-nightly-wheels/simple"'
 fi
 
+echo "Found:"
+echo " CC: $CC"
+echo " CXX: $CXX"
+
 # Ensure we can use a compiler
-conda list
-conda install gfortran
+conda install $conda_gfortran
 export FC=$(which gfortran)
+echo " FC: $FC"
+
+conda list
 
 # The version of the built dependencies are specified
 # in the pyproject.toml file, while the tests are run
