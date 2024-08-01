92cd5a266339ac8fc1a28548fe36b2aaa2634ad4
Nick Papior <nickpapior@gmail.com>
2024-08-01 09:32:58 +0200

    again trying to fix inadvertent dependencies
    Downstream packages has messed their dependencies,
    it is hard, and hence requires quite a bit of work.
    
    This commit also tries to add more checks in the CI
    to ensure a truly minimal run is processed.
    
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- src/sisl/physics/tests/test_brillouinzone.py
+++ src/sisl/physics/tests/test_brillouinzone.py
@@ -481,12 +481,23 @@ class TestMonkhorstPack:
         # try and determine a sensible
         import os
 
+        nprocs = 2
+
+        try:
+            nprocs = len(os.sched_getaffinity(0)) // 2
+        except Exception:
+            pass
+
         try:
             import psutil
 
             nprocs = len(psutil.Process().cpu_affinity()) // 2
         except Exception:
-            nprocs = len(os.sched_getaffinity(0)) // 2
+            pass
+
+        if nprocs == 1:
+            pytest.skip("not in a parallel environment")
+
         nprocs = max(2, nprocs)
         omp_num_threads = os.environ.get("OMP_NUM_THREADS")
 
@@ -496,7 +507,7 @@ class TestMonkhorstPack:
         papply = bz.apply.renew(pool=nprocs)
         assert str(apply) != str(papply)
 
-        for method in ["iter", "average", "sum", "array", "list", "oplist"]:
+        for method in ("iter", "average", "sum", "array", "list", "oplist"):
             # TODO One should be careful with zip
             # zip will stop when it hits the final element in the first
             # list.
@@ -518,7 +529,7 @@ class TestMonkhorstPack:
         papply = bz.apply.renew(pool=True).eigh
         assert str(apply) != str(papply)
 
-        for method in ["iter", "average", "sum", "array", "list", "oplist"]:
+        for method in ("iter", "average", "sum", "array", "list", "oplist"):
             # TODO One should be careful with zip
             # zip will stop when it hits the final element in the first
             # list.
