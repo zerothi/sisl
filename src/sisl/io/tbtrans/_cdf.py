Fixed error in bond_current calculation
Now there is better warning, and correct behaviour for
current calculations of orbital|bond|vector_current.

There has been no release with this bug, so it was
only in the development version.

Signed-off-by: Nick Papior <nickpapior@gmail.com>

src/sisl/io/tbtrans/_cdf.py
@@ -192,18 +192,19 @@ class _ncSileTBtrans(SileCDFTBtrans):
         Parameters
         ----------
         E :
-           if `int`, return it-self, else return the energy index which is
-           closests to the energy.
+           return the energy index which is
+           closest to the energy passed.
            For a `str` it will be parsed to a float and treated as such.
         method :
             how non-equal values should be located.
-            * `nearest` takes the closests value
-            * `above` takes the closests value above `E`.
-            * `below` takes the closests value below `E`.
+            * `nearest` takes the closest value
+            * `above` takes the closest value above `E`.
+            * `below` takes the closest value below `E`.
         """
-        warn(
-            f"{self.__class__.__name__}.Eindex handles int's the same as floats [>0.15.2]."
-        )
+        if isinstance(E, int):
+            warn(
+                f"{self.__class__.__name__}.Eindex handles int's the same as floats [>0.15.2]."
+            )
         E = float(E)
 
         dE = self.E - E
@@ -245,6 +246,11 @@ class _ncSileTBtrans(SileCDFTBtrans):
             )
         return idxE
 
+    def _argsort_E(self):
+        """Internal routine for returning energies and transmission in a sorted array"""
+        idx_sort = np.argsort(self.E)
+        return idx_sort
+
     def _bias_window_integrator(self, elec_from: ElecType = 0, elec_to: ElecType = 1):
         r"""An integrator for the bias window between two electrodes
 
@@ -272,7 +278,7 @@ class _ncSileTBtrans(SileCDFTBtrans):
         kt_to = self.kT(elec_to)
         mu_to = self.chemical_potential(elec_to)
         # Get energies
-        E = self._E_T_sorted(elec_from, elec_to)[0]
+        E = self.E[self._argsort_E()]
         dE = E[1] - E[0]
 
         def integrator(E):

src/sisl/io/tbtrans/tbt.py
@@ -765,14 +765,6 @@ class tbtncSileTBtrans(_devncSileTBtrans):
         else:
             return self._value_E("DOS", elec, kavg=kavg, E=E) * fact
 
-    def _E_T_sorted(self, elec_from, elec_to, kavg=True):
-        """Internal routine for returning energies and transmission in a sorted array"""
-        E = self.E
-        idx_sort = np.argsort(E)
-        # Get transmission
-        T = self.transmission(elec_from, elec_to, kavg)
-        return E[idx_sort], T[idx_sort]
-
     def current(
         self, elec_from: ElecType = 0, elec_to: ElecType = 1, kavg=True
     ) -> float:
@@ -852,7 +844,10 @@ class tbtncSileTBtrans(_devncSileTBtrans):
         elec_from = self._elec(elec_from)
         elec_to = self._elec(elec_to)
         # Get energies
-        E, T = self._E_T_sorted(elec_from, elec_to, kavg)
+        idx_sort = self._argsort_E()
+        T = self.transmission(elec_from, elec_to, kavg=kavg)
+        E = self.E[idx_sort]
+        T = T[idx_sort]
 
         dE = E[1] - E[0]
         window_warning(
@@ -1674,20 +1669,20 @@ class tbtncSileTBtrans(_devncSileTBtrans):
 
         # Do integration of data
         def func_out(data, A):
-            i, weight = A
-            D = self._sparse_data("J", elec, i, kavg=kavg)
+            E, weight = A
+            D = self._sparse_data("J", elec, E, kavg=kavg)
             D[D < 0] = 0
             return data + D * weight
 
         def func_in(data, A):
-            i, weight = A
-            D = self._sparse_data("J", elec, i, kavg=kavg)
+            E, weight = A
+            D = self._sparse_data("J", elec, E, kavg=kavg)
             D[D > 0] = 0
             return data + D * weight
 
         def func_all(data, A):
-            i, weight = A
-            D = self._sparse_data("J", elec, i, kavg=kavg)
+            E, weight = A
+            D = self._sparse_data("J", elec, E, kavg=kavg)
             return data + D * weight
 
         # nonlocal cannot be used in an if-statement
@@ -1710,7 +1705,7 @@ class tbtncSileTBtrans(_devncSileTBtrans):
                 "wrong value [all/both/+-/inout, +/out,-/in] allowed."
             )
 
-        J = reduce(getdata, enumerate(integrator(self.E)), 0.0)
+        J = reduce(getdata, zip(self.E, integrator(self.E)), 0.0)
 
         return (
             self._sparse_data_to_matrix(J, isc, orbitals)

src/sisl/io/tbtrans/tests/test_tbt.py
@@ -261,19 +261,22 @@ def test_1_graphene_all_content(sisl_files):
     DOS = tbt.DOS
     ADOS = tbt.ADOS
 
-    assert DOS(2, atoms=True, sum=False).size == geom.names["Device"].size
-    assert np.allclose(DOS(2, atoms="Device", sum=False), DOS(2, atoms=True, sum=False))
-    assert DOS(2, orbitals=True, sum=False).size == geom.a2o("Device", all=True).size
-    assert ADOS(left, 2, atoms=True, sum=False).size == geom.names["Device"].size
+    assert DOS(2.0, atoms=True, sum=False).size == geom.names["Device"].size
+    assert np.allclose(
+        DOS(2.0, atoms="Device", sum=False), DOS(2.0, atoms=True, sum=False)
+    )
+    assert DOS(2.0, orbitals=True, sum=False).size == geom.a2o("Device", all=True).size
+    assert ADOS(left, 2.0, atoms=True, sum=False).size == geom.names["Device"].size
     assert (
-        ADOS(left, 2, orbitals=True, sum=False).size
+        ADOS(left, 2.0, orbitals=True, sum=False).size
         == geom.a2o("Device", all=True).size
     )
     assert np.allclose(
-        ADOS(left, 2, atoms="Device", sum=False), ADOS(left, 2, atoms=True, sum=False)
+        ADOS(left, 2.0, atoms="Device", sum=False),
+        ADOS(left, 2.0, atoms=True, sum=False),
     )
 
-    for E in (None, 2, 3):
+    for E in (None, 2.0, 3.0):
         assert np.allclose(DOS(E), ADOS(left, E) + ADOS(right, E), atol=1e-5)
 
         for o, indices in (("atoms", range(8, 12)), ("orbitals", range(50, 70))):
@@ -293,7 +296,7 @@ def test_1_graphene_all_content(sisl_files):
             )
 
     # Check orbital currents
-    E = 3
+    E = 0.75
     # Sum of orbital current should be 0 (in == out)
     # However, when eta is *big*, this tends to get blurred.
     orb_left = tbt.orbital_transmission(E, left)
@@ -643,7 +646,7 @@ def test_1_graphene_all_sparse_data_isc_request(sisl_files):
 
     # request the full matrix
     for elec in [0, 1]:
-        J_all = tbt.orbital_transmission(3, elec)
+        J_all = tbt.orbital_transmission(0.75, elec)
         J_all.eliminate_zeros()
 
         # Ensure we actually have something
@@ -651,7 +654,7 @@ def test_1_graphene_all_sparse_data_isc_request(sisl_files):
 
         # partial summed isc
         # Test that the full matrix and individual access is the same
-        J_sum = sum(tbt.orbital_transmission(3, elec, isc=isc) for isc in sc.sc_off)
+        J_sum = sum(tbt.orbital_transmission(0.75, elec, isc=isc) for isc in sc.sc_off)
         assert J_sum.nnz == J_all.nnz
         assert (J_sum - J_all).nnz == 0
 
@@ -660,8 +663,8 @@ def test_1_graphene_all_sparse_data_orbitals(sisl_files):
     tbt = sisl.get_sile(sisl_files("siesta", "tbtrans", "graphene", "graphene.TBT.nc"))
 
     # request the full matrix
-    J_all = tbt.orbital_transmission(3, 0)
-    J_12 = tbt.orbital_transmission(3, 0, orbitals=[2, 3])
+    J_all = tbt.orbital_transmission(0.75, 0)
+    J_12 = tbt.orbital_transmission(0.75, 0, orbitals=[2, 3])
 
     assert J_12.nnz < J_all.nnz // 2
 
@@ -672,4 +675,4 @@ def test_projection_raise(sisl_files):
     )
 
     with pytest.raises(sisl.io.tbtrans.MissingFDFTBtransError):
-        tbt.orbital_transmission(2, 0)
+        tbt.orbital_transmission(1.995, 0)